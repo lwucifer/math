a754b748c334425635061c6a6ad436f981826a67
Nguyễn Quang Trung <trungnq@bcnex>
2019-12-24 18:47:22 +0700

    folder

---
+++ pages/course/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Course</h1>
+</template>
+
+<script>
+export default {
+    name: "School"
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/school/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>School</h1>
+</template>
+
+<script>
+export default {
+    name: "School"
+}
+</script>
+
+<style>
+
+</style>
