87f52f40d48db04f35a740e77241202788ed5f83
tiendatleo <tiendatleo218@gmail.com>
2019-12-30 12:24:24 +0700

    Add AppAvatar component

---
+++ components/App/AppAvatar.vue
@@ -0,0 +1,51 @@
+<template functional>
+  <span
+    :class="[
+      data.class,
+      data.staticClass,
+      'app-avatar',
+      props.shape === 'circle' && 'app-avatar-circle',
+      props.shape === 'square' && 'app-avatar-square',
+      props.size === 'sm' && 'app-avatar--size-sm',
+      props.size === 'lg' && 'app-avatar--size-lg',
+    ]"
+    :style="[
+      ['px', 'rem', '%', 'em'].every(char => props.size.toString().indexOf(char) > -1) && `width: ${props.size}px; height: ${props.size}px;`,
+    ]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
+    <img :src="props.src" :srcset="props.srcSet" :alt="props.alt" @error="props.loadError()" />
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    shape: {
+      type: String,
+      default: "circle" // circle | square,
+    },
+    size: {
+      type: [String, Number],
+      default: "default" // default | xs | sm | lg | css value
+    },
+    src: {
+      type: String
+    },
+    srcSet: {
+      type: String
+    },
+    alt: {
+      type: String
+    },
+    loadError: {
+      type: Function
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/components/app/_app-avatar.scss";
+</style>
--- plugins/global.js
+++ plugins/global.js
@@ -3,7 +3,9 @@ import Vue from "vue";
 import AppButton from "~/components/App/AppButton";
 import AppDivider from "~/components/App/AppDivider";
 import AppContentBox from "~/components/App/AppContentBox";
+import AppAvatar from "~/components/App/AppAvatar";
 
 Vue.component("app-button", AppButton);
 Vue.component("app-divider", AppDivider);
 Vue.component("app-content-box", AppContentBox);
+Vue.component("app-avatar", AppAvatar);
