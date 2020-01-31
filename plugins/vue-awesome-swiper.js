5acdfb15bb7ff4b229e21af61543b22d330e5850
tiendatleo <tiendatleo218@gmail.com>
2020-01-31 16:17:04 +0700

    swiper slider css import in component

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr'
-import 'swiper/dist/css/swiper.css'
+// import 'swiper/dist/css/swiper.css' - use this import in style tag of component
 
 Vue.use(VueAwesomeSwiper)
