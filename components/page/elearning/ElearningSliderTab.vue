b84911253f65a629fd3c03c8482b700939b61e32
luunv <luunguyen.mta@gmail.com>
2020-02-05 10:32:49 +0700

    Refactor code: UI pages

--- components/page/elearning/ElearningSliderTab.vue
+++ components/page/elearning/ElearningSliderTab.vue
@@ -1,112 +1,126 @@
 <template>
-    <div class="elearning-slider-tab">
-        <div class="toolbar">
-            <h3 v-if="title">{{title}}</h3>
-            <app-button @click="changeTab(1)" :color="tab === 1 ? 'primary' : 'info'" square v-if="sciences.length > 0">
-                <IconNote/>
-                Bài giảng
-            </app-button>
-            <app-button @click="changeTab(2)" :color="tab === 2 ? 'primary' : 'info'" square v-if="sciences.length > 0">
-                <IconBooks/>
-                Khóa học
-            </app-button>
-        </div>
-        <div v-swiper:mySwiper="currentSwiperOptions" class="post-slider elearning-slider" v-on="$listeners">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide elearning-slider-tab-container" v-for="(item, index) in list" :key="index">
-                    <div class="slider-item" @click="$emit('click-item', item, index)">
-                        <ElearningItem2 :item="item" />
-                    </div>
-                </div>
-            </div>
-
-            <div class="swiper-button-prev" v-if="currentSwiperOptions.navigation">
-                <IconChevronLeft/>
-            </div>
-            <div class="swiper-button-next" v-if="currentSwiperOptions.navigation">
-                <IconChevronRight/>
-            </div>
-            <div class="swiper-pagination" v-if="currentSwiperOptions.pagination"></div>
-        </div>
+  <div class="elearning-slider-tab">
+    <div class="toolbar">
+      <h3 v-if="title">{{title}}</h3>
+      <app-button
+        @click="changeTab(1)"
+        :color="tab === 1 ? 'primary' : 'info'"
+        square
+        v-if="sciences.length > 0"
+      >
+        <IconNote />Bài giảng
+      </app-button>
+      <app-button
+        @click="changeTab(2)"
+        :color="tab === 2 ? 'primary' : 'info'"
+        square
+        v-if="sciences.length > 0"
+      >
+        <IconBooks />Khóa học
+      </app-button>
     </div>
+    <div
+      v-swiper:mySwiper="currentSwiperOptions"
+      class="post-slider elearning-slider"
+      v-on="$listeners"
+    >
+      <div class="swiper-wrapper">
+        <div
+          class="swiper-slide elearning-slider-tab-container"
+          v-for="(item, index) in list"
+          :key="index"
+        >
+          <div class="slider-item" @click="$emit('click-item', item, index)">
+            <ElearningItem2 :item="item" />
+          </div>
+        </div>
+      </div>
+
+      <div class="swiper-button-prev" v-if="currentSwiperOptions.navigation">
+        <IconChevronLeft />
+      </div>
+      <div class="swiper-button-next" v-if="currentSwiperOptions.navigation">
+        <IconChevronRight />
+      </div>
+      <div class="swiper-pagination" v-if="currentSwiperOptions.pagination"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import {assignIn} from "lodash";
-    import ElearningItem2 from "~/components/page/elearning/ElearningItem2";
-    import IconChevronLeft from "~/assets/svg/icons/chevron-left.svg?inline";
-    import IconChevronRight from "~/assets/svg/icons/chevron-right.svg?inline";
-    import IconBooks from "~/assets/svg/icons/books.svg?inline";
-    import IconNote from "~/assets/svg/icons/note.svg?inline";
-    import IconStar from "~/assets/svg/icons/star.svg?inline";
-    import IconStarO from "~/assets/svg/icons/star-o.svg?inline";
+import { assignIn } from "lodash";
+import ElearningItem2 from "~/components/page/elearning/ElearningItem2";
+import IconChevronLeft from "~/assets/svg/icons/chevron-left.svg?inline";
+import IconChevronRight from "~/assets/svg/icons/chevron-right.svg?inline";
+import IconBooks from "~/assets/svg/icons/books.svg?inline";
+import IconNote from "~/assets/svg/icons/note.svg?inline";
+import IconStar from "~/assets/svg/icons/star.svg?inline";
+import IconStarO from "~/assets/svg/icons/star-o.svg?inline";
 
-    export default {
-        components: {
-            IconChevronLeft,
-            IconChevronRight,
-            IconBooks,
-            IconNote,
-            IconStarO,
-            IconStar,
-            ElearningItem2
-        },
+export default {
+  components: {
+    IconChevronLeft,
+    IconChevronRight,
+    IconBooks,
+    IconNote,
+    IconStarO,
+    IconStar,
+    ElearningItem2
+  },
 
-        props: {
-            lessons: {
-                type: Array,
-                required: true,
-                default: () => []
-            },
+  props: {
+    lessons: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
 
-            sciences: {
-                type: Array,
-                required: true,
-                default: () => []
-            },
+    sciences: {
+      type: Array,
+      required: true,
+      default: () => []
+    },
 
-            swiperOptions: {
-                type: Object,
-                default: () => {
-                }
-            },
-            title: {type: String},
-        },
+    swiperOptions: {
+      type: Object,
+      default: () => {}
+    },
+    title: { type: String }
+  },
 
-        data() {
-            const defaultSwiperOptions = {
-                slidesPerView: "auto",
-                spaceBetween: 4,
-                navigation: {
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev"
-                },
-                pagination: false,
-                showName: false
-            };
-
-            return {
-                defaultSwiperOptions,
-                currentSwiperOptions: assignIn(defaultSwiperOptions, this.swiperOptions),
-                tab: 1,
-                list: [],
-            };
-        },
-
-        methods: {
-            changeTab(tab) {
-                this.tab = tab;
-                this.list = tab === 1 ? [...this.lessons] : [...this.sciences];
-            }
-        },
-
-        created() {
-            this.list = this.lessons;
-        }
-
+  data() {
+    const defaultSwiperOptions = {
+      slidesPerView: "auto",
+      spaceBetween: 4,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      pagination: false,
+      showName: false
     };
+
+    return {
+      defaultSwiperOptions,
+      currentSwiperOptions: assignIn(defaultSwiperOptions, this.swiperOptions),
+      tab: 1,
+      list: []
+    };
+  },
+
+  methods: {
+    changeTab(tab) {
+      this.tab = tab;
+      this.list = tab === 1 ? [...this.lessons] : [...this.sciences];
+    }
+  },
+
+  created() {
+    this.list = this.lessons;
+  }
+};
 </script>
 
 <style lang="scss">
-    @import "~/assets/scss/components/elearning/_elearning-slider-tab.scss";
+@import "~/assets/scss/components/elearning/_elearning-slider-tab.scss";
 </style>
--- pages/elearning/_id/index.vue
+++ pages/elearning/_id/index.vue
@@ -1,145 +1,157 @@
 <template>
-    <div class="container">
-        <h1>{{title}}</h1>
-        <div class="info">
-        </div>
+  <div class="container">
+    <h1>{{title}}</h1>
+    <div class="info"></div>
 
-        <ElearningSliderTab :lessons="sciences" :swiperOptions="sliderOptions" title="Bài giảng cùng giáo viên"/>
-        <ElearningSliderTab :lessons="sciences" :swiperOptions="sliderOptions" title="Bài giảng liên quan" class="mt-5"/>
-    </div>
+    <ElearningSliderTab
+      :lessons="sciences"
+      :swiperOptions="sliderOptions"
+      title="Bài giảng cùng giáo viên"
+    />
+    <ElearningSliderTab
+      :lessons="sciences"
+      :swiperOptions="sliderOptions"
+      title="Bài giảng liên quan"
+      class="mt-5"
+    />
+  </div>
 </template>
 
 <script>
-    import IconEye from "~/assets/svg/icons/eye.svg?inline";
-    import IconStar from "~/assets/svg/icons/star.svg?inline";
-    import IconStarO from "~/assets/svg/icons/star-o.svg?inline";
-    import ElearningSliderTab from "~/components/page/elearning/ElearningSliderTab";
-    import {mapState} from 'vuex';
-    import * as actionTypes from '~/utils/action-types';
+import IconEye from "~/assets/svg/icons/eye.svg?inline";
+import IconStar from "~/assets/svg/icons/star.svg?inline";
+import IconStarO from "~/assets/svg/icons/star-o.svg?inline";
+import ElearningSliderTab from "~/components/page/elearning/ElearningSliderTab";
+import { mapState } from "vuex";
+import * as actionTypes from "~/utils/action-types";
 
-    export default {
-        name: "E-learning",
+export default {
+  name: "E-learning",
 
-        components: {
-            ElearningSliderTab,
-            IconStarO,
-            IconStar,
-            IconEye,
+  components: {
+    ElearningSliderTab,
+    IconStarO,
+    IconStar,
+    IconEye
+  },
+
+  data() {
+    return {
+      isAuthenticated: true,
+      title: "Làm chủ 6 công cụ Marketing online HOT NHẤT hiện nay",
+      sciences: [
+        {
+          id: 1,
+          name: "2 Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
         },
-
-        data() {
-            return {
-                isAuthenticated: true,
-                title: 'Làm chủ 6 công cụ Marketing online HOT NHẤT hiện nay',
-                sciences: [
-                    {
-                        id: 1,
-                        name: '2 Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },{
-                        id: 2,
-                        name: '2 Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },{
-                        id: 3,
-                        name: '2 Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },{
-                        id: 4,
-                        name: 'Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },{
-                        id: 5,
-                        name: 'Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },{
-                        id: 6,
-                        name: 'Nền tảng tiếng Anh cho người mới bắt đầu',
-                        image: 'https://picsum.photos/218/130',
-                        price: '219000',
-                        online: 0,
-                        onlineStatus: 'Thời gian học kế tiếp 11:50 AM, 10/12/2019',
-                        teacher: {
-                            id: 1,
-                            name: 'Trần Văn A',
-                            avatar: 'https://picsum.photos/20/20',
-                            star: 4,
-                            starAmount: 476,
-                        }
-                    },
-                ],
-                sliderOptions: {
-                    spaceBetween: 20,
-                    slidesPerView: 5,
-                    setWrapperSize: true,
-                    autoHeight: true,
-                    watchOverflow: true,
-                    showName: true
-                },
-                active_el: 0
-            };
+        {
+          id: 2,
+          name: "2 Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
         },
-        computed: {
-            ...mapState("auth", ["loggedUser"]),
+        {
+          id: 3,
+          name: "2 Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
         },
-
-        watch: {},
-
-        methods: {
+        {
+          id: 4,
+          name: "Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
+        },
+        {
+          id: 5,
+          name: "Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
+        },
+        {
+          id: 6,
+          name: "Nền tảng tiếng Anh cho người mới bắt đầu",
+          image: "https://picsum.photos/218/130",
+          price: "219000",
+          online: 0,
+          onlineStatus: "Thời gian học kế tiếp 11:50 AM, 10/12/2019",
+          teacher: {
+            id: 1,
+            name: "Trần Văn A",
+            avatar: "https://picsum.photos/20/20",
+            star: 4,
+            starAmount: 476
+          }
         }
+      ],
+      sliderOptions: {
+        spaceBetween: 20,
+        slidesPerView: 5,
+        setWrapperSize: true,
+        autoHeight: true,
+        watchOverflow: true,
+        showName: true
+      },
+      active_el: 0
     };
+  },
+  computed: {
+    ...mapState("auth", ["loggedUser"])
+  },
+
+  watch: {},
+
+  methods: {}
+};
 </script>
 
-<style>
-    @import "~/assets/scss/components/course/_course.scss";
+<style lang="scss">
+@import "~/assets/scss/components/course/_course.scss";
 </style>
