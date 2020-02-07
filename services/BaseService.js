77655d180c54d69b1de875392b135402389615b9
huydv <dohuy2611@gmail.com>
2020-02-07 12:02:24 +0700

    fix-login

--- services/BaseService.js
+++ services/BaseService.js
@@ -1,46 +1,44 @@
-import * as actionTypes from '../utils/action-types';
+import * as actionTypes from "../utils/action-types";
 
 export default class BaseService {
+    constructor($axios, $api) {
+        this.$axios = $axios;
+        this.$api = $api;
+    }
 
-  constructor($axios, $api) {
-    this.$axios = $axios;
-    this.$api = $api;
-  }
+    async [actionTypes.BASE.LIST](payload) {
+        const { data } = await this.$axios.get(this.$api, payload);
 
-  async [actionTypes.BASE.LIST](payload) {
-    const {
-      data
-    } = await this.$axios.get(this.$api, payload);
+        const result = data ? data : {};
+        console.log("[BASE.LIST] result", result);
+        return result;
+    }
 
-    const result = data ? data : {};
-    console.log("[BASE.LIST] result", result);
-    return result;
-  }
+    async [actionTypes.BASE.EDIT](id, payload) {
+        console.log("[BaseService] params", id, payload);
+        const { data } = await this.$axios.put(`${this.$api}/${id}`, payload);
 
-  async [actionTypes.BASE.EDIT](id, payload) {
-    console.log("[BaseService] params", id, payload);
-    const {
-      data
-    } = await this.$axios.put(`${this.$api}/${id}`, payload);
+        return data;
+    }
 
-    return data;
-  }
+    async [actionTypes.BASE.ADD](payload) {
+        const { data } = await this.$axios.post(this.$api, payload);
 
-  async [actionTypes.BASE.ADD](payload) {
-    const {
-      data
-    } = await this.$axios.post(this.$api, payload);
+        return data;
+    }
 
-    return data;
-  }
+    async [actionTypes.BASE.DELETE](payload) {
+        const { data } = await this.$axios.delete(this.$api, payload);
 
-  async [actionTypes.BASE.DELETE](payload) {
-    const {
-      data
-    } = await this.$axios.delete(this.$api, payload);
+        return data;
+    }
+    async [actionTypes.BASE.DETAIL](id) {
+        const { data } = await this.$axios.get(`${this.$api}/${id}`);
 
-    return data;
-  }
+        const result = data ? data : {};
 
+        // console.log("[BASE.DETAIL]", result);
 
+        return result;
+    }
 }
