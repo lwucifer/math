ca350f902026ea69e58167dd8920a4e702ade1bd
luunv <luunguyen.mta@gmail.com>
2020-02-07 17:52:30 +0700

    Add UI Auth pages

--- components/page/account/AccountFriendsRequests.vue
+++ components/page/account/AccountFriendsRequests.vue
@@ -1,108 +1,105 @@
 <template>
-	<div class="account-friends">
-		<div class="account-friends__title d-block mb-4">
-			<h3>Bạn bè</h3>
-			<p class="color-999 mt-2">Bạn có 87 lời mời kết bạn</p>
-		</div>
-		<div class="account-friends__list">
-			<div class="row">
-				<div class="col-md-6 col-xs-12" v-for="(item, index) in friends" :key="index">
-					<AccountFriendsRequestsItem :data="item"/>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="account-friends">
+    <div class="account-friends__title d-block mb-4">
+      <h3>Bạn bè</h3>
+      <p class="color-999 mt-2">Bạn có 87 lời mời kết bạn</p>
+    </div>
+    <div class="account-friends__list">
+      <div class="row">
+        <div class="col-md-6 col-xs-12" v-for="(item, index) in friends" :key="index">
+          <AccountFriendsRequestsItem :data="item" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	import AccountFriendsRequestsItem from "~/components/page/account/AccountFriendsRequestsItem";
+import AccountFriendsRequestsItem from "~/components/page/account/AccountFriendsRequestsItem";
 
-	export default {
-		components: {
-			AccountFriendsRequestsItem,
-		},
+export default {
+  components: {
+    AccountFriendsRequestsItem
+  },
 
-		data() {
-			return {
-				name: '',
-				friends: [
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				{
-					id: 1,
-					name: 'Scarlett Black',
-					mutual_friends: 69,
-					avatar: 'https://picsum.photos/100/100',
-				},
-				],
-			}
-		},
+  data() {
+    return {
+      name: "",
+      friends: [
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        },
+        {
+          id: 1,
+          name: "Scarlett Black",
+          mutual_friends: 69,
+          avatar: "https://picsum.photos/100/100"
+        }
+      ]
+    };
+  },
 
-		computed: {
-		},
+  computed: {},
 
-		mounted() {},
-
-		methods: {}
-	};
+  methods: {}
+};
 </script>
 
 <style lang="scss">
---
+++ pages/auth/forgot/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="auth auth-forgot">
+    <div class="auth__main">
+      <h3>Quên mật khẩu?</h3>
+      <div class="auth_content mt-5">
+        <div class="form-group">
+          <input
+            type="text"
+            v-model="email"
+            class="form-control"
+            :class="error ? 'border-red' : ''"
+            placeholder="Nhập số điện thoại hoặc email"
+          />
+          <p class="font-size-12 color-red text-left full-width mt-2" v-if="error">Tài khoản không tồn tại</p>
+        </div>
+        <app-button color="primary" square fullWidth @click="login" class="mb-3">Khôi phục mật khẩu</app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as actionTypes from "~/utils/action-types";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  components: {},
+
+  data() {
+    return {
+      phone: "",
+      email: "",
+      password: "",
+      error: false
+    };
+  },
+
+  methods: {
+    ...mapActions("auth", ["login"]),
+    login() {
+      this.error = true;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/components/auth/_auth.scss";
+</style>
