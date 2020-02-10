a1b2c5f5326dd7aca2dba619c2a8d0dc119c0275
tiendatleo <tiendatleo218@gmail.com>
2020-02-10 14:25:26 +0700

    add vuelidate package

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,8 @@ module.exports = {
         { src: "@/plugins/click-outside.js", ssr: false },
         { src: "@/plugins/global.js" },
         { src: "@/plugins/vue-awesome-swiper.js", ssr: false },
-        { src: "@/plugins/textarea-autosize.js", ssr: false }
+        { src: "@/plugins/textarea-autosize.js", ssr: false },
+        { src: "@/plugins/vuelidate.js", ssr: true },
         // { src: "@/plugins/firebase-auth.js"},
     ],
     /**
