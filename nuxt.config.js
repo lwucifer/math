3f77335bf7a65ef0edb2602b39c7ebdb55d75fd9
Nguyễn Quang Trung <trungnq@bcnex>
2020-02-11 15:10:29 +0700

    change config fb

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,8 @@ module.exports = {
         { src: "@/plugins/vue-awesome-swiper.js", ssr: false },
         { src: "@/plugins/textarea-autosize.js", ssr: false },
         { src: "@/plugins/vuelidate.js", ssr: true },
-        { src: "@/plugins/vue-moment.js" }
-        // { src: "@/plugins/firebase-auth.js"},
+        { src: "@/plugins/vue-moment.js" },
+        { src: "@/plugins/firebase-auth.js"},
     ],
     /**
      * Global middleware
--- services/firebase/FirebaseInit.js
+++ services/firebase/FirebaseInit.js
@@ -3,15 +3,25 @@ import "firebase/auth";
 import "firebase/database";
 
 // Your web app's Firebase configuration
+// console.log("[process.env]", process.env);
 var firebaseConfig = {
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    databaseURL: process.env.FB_DB_URL,
-    projectId: process.env.FB_PROJECT_ID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-    appId: process.env.FB_APP_ID,
-    measurementId: process.env.FB_MEASUREMENT_ID
+    apiKey: "AIzaSyBjG_rG7dp2-IqxI_28nUfG5MlcpiNpjmo",
+    authDomain: "schoolly-37aa8.firebaseapp.com",
+    databaseURL: "https://schoolly-37aa8.firebaseio.com",
+    projectId: "schoolly-37aa8",
+    storageBucket: "schoolly-37aa8.appspot.com",
+    messagingSenderId: "590078046901",
+    appId: "1:590078046901:web:d30773daf7967d8d1b84fc",
+    measurementId: "G-LN58DKHESW"
+
+    // apiKey: process.env.FB_API_KEY,
+    // authDomain: process.env.FB_AUTH_DOMAIN,
+    // databaseURL: process.env.FB_DB_URL,
+    // projectId: process.env.FB_PROJECT_ID,
+    // storageBucket: process.env.FB_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    // appId: process.env.FB_APP_ID,
+    // measurementId: process.env.FB_MEASUREMENT_ID
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : "";
