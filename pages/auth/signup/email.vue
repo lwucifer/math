92aad903af4f032e77eaa6770b771bb1e364154d
luunv <luunguyen.mta@gmail.com>
2020-02-11 12:00:28 +0700

    Add auth pages

---
+++ pages/auth/signup/email.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="auth auth-signin">
+    <div class="auth__main">
+      <h3>Đăng ký thành công</h3>
+      <div class="auth_content mt-4 mb-4 text-center">
+        <p class="line-height-2">
+            Tài khoản của bạn đã đăng ký thành công!<br>
+            Vui lòng xác thực tài khoản qua email<br> 
+            <span class="color-primary">datpt262@gmail.com</span>
+        </p>
+      </div>
+      <app-button color="primary" square fullWidth @click="login">
+          Xác thực tài khoản
+      </app-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as actionTypes from "~/utils/action-types";
+import { mapState, mapActions } from "vuex";
+
+export default {
+  components: {},
+
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+    ...mapActions("auth", ["login"]),
+    login() {
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/components/auth/_auth.scss";
+</style>
--- pages/auth/signup/index.vue
+++ pages/auth/signup/index.vue
@@ -34,7 +34,6 @@
 </template>
 
 <script>
-import IconSuccess from "~/assets/svg/icons/success.svg?inline";
 import IconFacebook from "~/assets/svg/icons/facebook.svg?inline";
 import IconGoogle from "~/assets/svg/icons/google.svg?inline";
 import * as actionTypes from "~/utils/action-types";
@@ -43,8 +42,7 @@ import { mapState, mapActions } from "vuex";
 export default {
   components: {
     IconFacebook,
-    IconGoogle,
-    IconSuccess
+    IconGoogle
   },
 
   data() {
