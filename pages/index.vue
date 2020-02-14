f46151c7fd6e8f3157ee7a805c98eb37775d0554
tiendatleo <tiendatleo218@gmail.com>
2020-02-14 11:23:30 +0700

    fake data timeline

--- pages/index.vue
+++ pages/index.vue
@@ -234,6 +234,7 @@ import { mapState, mapGetters } from "vuex";
 import * as actionTypes from "~/utils/action-types";
 import { POST_TYPES, LIKE_SOURCE_TYPES, LIKE_TYPES } from "~/utils/constants";
 import { createLike } from "~/models/social/Like";
+import { MESSAGES, COURSES_LIST, TIMELINE_SLIDER_ITEMS } from "~/server/fakedata/timeline";
 
 import SliderBanner from "~/components/page/timeline/slider/SliderBanner";
 import PostEditor from "~/components/page/timeline/postEditor/PostEditor";
@@ -273,24 +274,6 @@ export default {
       coursesTab: 0,
       modalDetailShow: false,
       dataModalDetail: {},
-      messages: [
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Mr. Damian",
-          desc: "Tiếng Trung cơ bản cho người mới bắt đầu 1"
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Nguyễn Đăng Dũng",
-          desc:
-            "Nắm được các kỹ năng cơ bản của giao tiếp: kỹ năng nghe, nói, khen chê và phi ngôn ngữ."
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Trần Quyền",
-          desc: "Tiếng Trung cơ bản cho người mới bắt đầu 1"
-        }
-      ],
       coursesTabsList: [
         {
           text: "Miễn phí",
@@ -301,75 +284,11 @@ export default {
           value: 1
         }
       ],
-      coursesList: [
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Chiến lược tài chính",
-          desc: "Ts. Lê Thẩm Dương"
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Thực hành làm kế toán tổng hợp trên phầm mềm Misa",
-          desc: "Nguyễn Lê Hoàng"
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Chiến lược tài chính",
-          desc: "Ts. Lê Thẩm Dương"
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Chiến lược tài chính",
-          desc: "Ts. Lê Thẩm Dương"
-        },
-        {
-          image: "https://picsum.photos/64/64",
-          title: "Chiến lược tài chính",
-          desc: "Ts. Lê Thẩm Dương"
-        }
-      ],
-      timelineSliderItems: [
-        {
-          id: 0,
-          type: "image",
-          src: "https://picsum.photos/171/171"
-        },
-        {
-          id: 1,
-          type: "video",
-          src: "https://picsum.photos/1920/1080"
-        },
-        {
-          id: 2,
-          type: "image",
-          src: "https://picsum.photos/1024/768"
-        },
-        {
-          id: 3,
-          type: "image",
-          src: "https://picsum.photos/180/180"
-        },
-        {
-          id: 4,
-          type: "image",
-          src: "https://picsum.photos/200/200"
-        },
-        {
-          id: 5,
-          type: "image",
-          src: "https://picsum.photos/350/350"
-        },
-        {
-          id: 6,
-          type: "image",
-          src: "https://picsum.photos/400/400"
-        },
-        {
-          id: 7,
-          type: "image",
-          src: "https://picsum.photos/240/240"
-        }
-      ]
+
+      //Fake data
+      messages: MESSAGES,
+      coursesList: COURSES_LIST,
+      timelineSliderItems: TIMELINE_SLIDER_ITEMS
     };
   },
 
@@ -504,9 +423,12 @@ export default {
 
     async likePost(id, cb) {
       const likeModel = createLike(id, LIKE_SOURCE_TYPES.POST, LIKE_TYPES.LIKE);
-      const doLike = await this.$store.dispatch(`social/${actionTypes.SOCIAL_LIKES.ADD}`, likeModel);
-      console.log('likePost', doLike)
-      this.$store.dispatch(`social/${actionTypes.SOCIAL_FEEDS.LIST}`)
+      const doLike = await this.$store.dispatch(
+        `social/${actionTypes.SOCIAL_LIKES.ADD}`,
+        likeModel
+      );
+      console.log("likePost", doLike);
+      this.$store.dispatch(`social/${actionTypes.SOCIAL_FEEDS.LIST}`);
 
       // Have to run cb
       cb();
