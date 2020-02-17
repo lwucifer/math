f578de871dde965a8b4f8475a5e7fda2333aa386
ngduy <duy.nv.96@gmail.com>
2020-02-17 14:01:12 +0700

    add page show school's detail

---
+++ plugins/filters.js
@@ -0,0 +1,18 @@
+import Vue from "vue"
+
+/**
+ * 10000 => "10,000"
+ * @param {number} num
+ */
+function toThousandFilter(num, separator = '.') {
+    return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, separator))
+}
+
+const FILTERS = {
+    toThousandFilter: toThousandFilter
+}
+
+// register global utility filters
+Object.keys(FILTERS).forEach(key => {
+    Vue.filter(key, FILTERS[key])
+})
