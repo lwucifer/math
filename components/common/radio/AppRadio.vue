0aa0b4cc257b1bfe1f756b5c58a5df14df9c632d
tiendatleo <tiendatleo218@gmail.com>
2020-02-17 12:59:24 +0700

    add AppRadio component

---
+++ components/common/radio/AppRadio.vue
@@ -0,0 +1,22 @@
+<template>
+  <label class="app-radio">
+    <input type="radio" class="app-radio__input" v-bind="$attrs" v-on="$listeners" />
+    <span class="app-radio__decor">
+      <span class="app-radio__decor__inner"></span>
+    </span>
+
+    <span class="app-radio__text">
+      <slot />
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/components/app/_app-radio.scss";
+</style>
