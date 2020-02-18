0b4e5cf3173168d7553ef7ee933300df05dfdf77
huydv <dohuy2611@gmail.com>
2020-02-18 09:48:26 +0700

    validate singup email

--- utils/error-code.js
+++ utils/error-code.js
@@ -5,5 +5,9 @@ export const ERRORS = {
         USER_LOCKED: "SCLU_1005",
         BAD_CREDEN: "SCLCOM_1000",
         EMAIL_LEFT: "SCLU_1006"
+    },
+    REGISTER: {
+        EMAIL_PHONE_USED: "SCLU_1000",
+        PASSWORD_LEAST: "SCLU_1001"
     }
 };
