7803986a6183fccf61dc35ace1f61aadaddc4035
Nguyễn Quang Trung <trungnq@bcnex>
2020-02-19 19:05:25 +0700

    streamedian

---
+++ pages/test/rtsp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="player-container">
+    <h2>RTSP Streaming video</h2>
+    <video id="test_video" controls autoplay>
+      <source src="rtsp://222.254.35.170:8900/live" />
+    </video>
+  </div>
+</template>
+
+<script>
+// trungnqkey
+// rtmp://localhost:1935/live
+export default {
+  naem: "Streaming",
+
+  head() {
+    return {
+      script: [
+        {
+          src: "/vendors/streamedian.min.js",
+          type: "text/javascript",
+          defer: true
+        }
+      ]
+    };
+  },
+
+  data() {
+    return {
+      myPlayer: null,
+      mute: false
+    };
+  },
+
+  mounted() {
+    console.log("[mounted]");
+  },
+
+  methods: {}
+};
+</script>
+
+<style>
+</style>
