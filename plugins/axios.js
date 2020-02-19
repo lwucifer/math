82df4f4f54d7a626e5d43c27a3f01bff59bb76ba
huydv <dohuy2611@gmail.com>
2020-02-19 10:13:01 +0700

    add code 415

--- plugins/axios.js
+++ plugins/axios.js
@@ -20,10 +20,10 @@ export default function({ store, $axios, redirect }) {
         if (checkRequestAuthorize(config.url)) {
             if (!store.getters["auth/token"]) return;
             config.headers.common["Authorization"] = `Bearer ${
-              config.url.includes("/user/public/renew")
-                ? store.state.auth.token.token
-                : store.state.auth.access_token
-            }`;
+        config.url.includes("/user/public/renew")
+          ? store.state.auth.token.token
+          : store.state.auth.access_token
+      }`;
 
             // config.headers.common["Authorization"] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo0LCJwaG9uZV9udW1iZXIiOiIwMzU2MjU3MzI1In0sImlhdCI6MTU3NTUzNDcxOSwiZXhwIjoxODM0NzM0NzE5fQ.w-oB2pH2aPiyzTXpSQumuShy5xQQEGfURDp1-KjzfkM`;
         } else {
@@ -79,7 +79,7 @@ export default function({ store, $axios, redirect }) {
                 });
             });
             return retryOriginalRequest;
-        } else if (code === 422 || code === 403) {
+        } else if (code === 422 || code === 403 || code === 415) {
             store.commit(`auth/${MUTATION_AUTH.REMOVE_TOKEN}`);
             redirect(`/auth/signin`);
         } else {
