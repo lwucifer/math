894ab12fcef22ea08f2402aaff8a7fcbb709e293
tiendatleo <tiendatleo218@gmail.com>
2020-02-20 10:54:23 +0700

    update UI

--- components/page/course/CreateLesson/CreateExcercise.vue
+++ components/page/course/CreateLesson/CreateExcercise.vue
@@ -113,16 +113,16 @@
         <div class="ce-item d-flex align-items-center justify-content-between">
           <div class="ce-item__left d-flex align-items-center">
             <h3 class="body-2 mr-3">Bài tập 1</h3>
-            <a href="" class="ce-item__action edit mr-3">
+            <a href class="ce-item__action edit mr-3">
               <IconEditAlt class="icon d-block subheading fill-primary" />
             </a>
-            <a href="" class="ce-item__action delete mr-3">
+            <a href class="ce-item__action delete mr-3">
               <IconTrashAlt class="icon d-block subheading fill-secondary" />
             </a>
           </div>
 
           <div class="ce-item__right">
-            <a href="" class="text-secondary">Thêm câu hỏi</a>
+            <a href class="text-secondary">Thêm câu hỏi</a>
           </div>
         </div>
       </div>
@@ -134,24 +134,117 @@
       </div>
 
       <div class="cc-panel__body">
-        
+        <div class="cc-box">
+          <div class="cc-box__head">
+            <div class="cc-box__head-left">
+              <h2 class="cc-box__title heading-6">Bài tập 1</h2>
+              <button class="cc-box__btn cc-box__btn-edit">
+                <IconEditAlt class="icon" />
+              </button>
+            </div>
+          </div>
+
+          <div class="cc-box__body">
+            <div class="cc-box__bg-gray pa-4">
+              <label class="d-inline-block mb-3" for="question-editor">Nội dung câu hỏi</label>
+
+              <client-only>
+                <editor-content
+                  :editor="editor"
+                  class="editor cc-editor mb-4"
+                  id="question-editor"
+                />
+              </client-only>
+
+              <div class="row mb-4">
+                <div class="col-md-3">
+                  <label class="d-inline-block mb-3" for="answer-a">Đáp án đúng</label>
+                  <div>
+                    <app-radio name="answer" value="a" id="answer-a">A</app-radio>
+                  </div>
+                </div>
+
+                <div class="col-md-9">
+                  <label class="d-inline-block mb-3" for="answer-editor">Nội dung đáp án</label>
+                  <div class="d-flex align-items-start">
+                    <div class="flex-grow mr-4">
+                      <client-only>
+                        <editor-content
+                          :editor="answerEditor"
+                          class="editor cc-editor"
+                          id="answer-editor"
+                        />
+                      </client-only>
+                    </div>
+
+                    <div>
+                      <button>
+                        <IconTrashAlt class="icon d-block subheading fill-secondary" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="d-flex justify-content-end">
+                <app-button color="disabled" class="font-weight-semi-bold mr-4" size="sm" square>Huỷ bỏ</app-button>
+                <app-button color="primary" class="font-weight-semi-bold" size="sm" square>Lưu câu hỏi</app-button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Editor, EditorContent } from "tiptap";
+import { Placeholder } from "tiptap-extensions";
+
 import IconInfoCircle from "~/assets/svg/design-icons/info-circle.svg?inline";
 import IconAngleDown from "~/assets/svg/design-icons/angle-down.svg?inline";
-import IconEditAlt from '~/assets/svg/design-icons/edit-alt.svg?inline';
-import IconTrashAlt from '~/assets/svg/design-icons/trash-alt.svg?inline';
+import IconEditAlt from "~/assets/svg/design-icons/edit-alt.svg?inline";
+import IconTrashAlt from "~/assets/svg/design-icons/trash-alt.svg?inline";
 
 export default {
   components: {
     IconInfoCircle,
     IconAngleDown,
     IconEditAlt,
-    IconTrashAlt
+    IconTrashAlt,
+    EditorContent
+  },
+
+  data() {
+    return {
+      editor: null,
+      answerEditor: null
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        new Placeholder({
+          showOnlyCurrent: false,
+          emptyNodeText: "Khung Soạn thảo"
+        })
+      ]
+    });
+
+    this.answerEditor = new Editor({
+      extensions: [
+        new Placeholder({
+          showOnlyCurrent: false,
+          emptyNodeText: "Nội dung đáp án"
+        })
+      ]
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
   }
 };
 </script>
