492059df6889255b0e40992a3e2125188f1e8c60
huydv <dohuy2611@gmail.com>
2020-02-27 11:56:50 +0700

    update-profile

--- utils/config.js
+++ utils/config.js
@@ -5,5 +5,6 @@ export const SCHOOLLY_ACCESS_TOKEN = "schoolly_access_token";
 
 export const DATE_BIRTHDAY = "DD/MM/YYYY";
 export const DATE_YYYY_MM_DD = "YYYY/MM/DD";
+export const DATE_FORMAT = "YYYY-MM-DD";
 
 export const UNAUTHORIZE_API = ["user/public/renew", "user/public/login"];
