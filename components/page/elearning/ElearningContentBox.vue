a22a6cc4802d7b492ebe1be2750e1e9de40d076a
ngduy <duy.nv.96@gmail.com>
2020-02-28 16:16:53 +0700

    Add frame for page create classroom

---
+++ components/page/elearning/ElearningContentBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="elearning-content-box">
+    <div class="elearning-content-box__title">
+      <slot name="head">
+        <h5 class="color-primary mb-3">Bài tập và bài kiểm tra</h5>
+      </slot>
+      <hr class/>
+    </div>
+
+    <div class="elearning-content-box__main">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~/assets/scss/components/elearning/_elearning-content-box.scss";
+</style>
