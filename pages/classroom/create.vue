a43a5eaeec92c3aeb26448852c90d1c4da8595bb
ngduy <duy.nv.96@gmail.com>
2020-03-01 20:43:47 +0700

    Add create exercise

--- pages/classroom/create.vue
+++ pages/classroom/create.vue
@@ -8,13 +8,11 @@
         />
       </div>
       <div class="col-md-9">
-        <div class="elearning-manager-content">
-          <keep-alive>
-            <component
-              :is="currentContent"
-            >
-            </component>
-          </keep-alive>
+        <div class="manager-content">
+          <create-classroom v-if="actived == 'classroom'"></create-classroom>
+          <create-exercise v-if="actived == 'exercise'"></create-exercise>
+<!--          <create-classroom v-if="actived == 'classroom'"></create-classroom>-->
+<!--          <create-classroom v-if="actived == 'classroom'"></create-classroom>-->
         </div>
       </div>
     </div>
@@ -23,30 +21,27 @@
 
 <script>
   import CreateAside from "~/components/page/classroom/create/CreateAside"
+  import CreateClassroom from "~/components/page/classroom/create/CreateClassroom"
+  import CreateExercise from "~/components/page/classroom/create/CreateExercise"
+
   import {mapState} from "vuex";
   import * as actionTypes from "~/utils/action-types";
-
-  const CreateClassroom = () => import("~/components/page/classroom/create/CreateClassroom")
 
   export default {
     layout: 'create',
     components: {
       CreateAside,
-      CreateClassroom
+      CreateClassroom,
+      CreateExercise,
     },
 
     data() {
       return {
-        actived: 'classroom'
+        actived: 'exercise'
       }
     },
     computed: {
-      currentContent: function() {
-        const MATCHED_CONTENT = {
-          classroom: 'CreateClassroom'
-        }
-        return MATCHED_CONTENT[this.actived]
-      }
+
     },
 
     methods: {
@@ -57,6 +52,6 @@
   };
 </script>
 
-<style lang="scss" scoped>
-  @import "~/assets/scss/components/elearning/manager/_elearning-manager-content.scss";
+<style lang="scss">
+  @import "~/assets/scss/components/classroom/_classroom-create.scss";
 </style>
