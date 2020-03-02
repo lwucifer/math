f5c9c611d959d75b19b99e1d5c4a63358e6d9b4e
chungpd <nh0crio96@gmail.com>
2020-03-02 08:46:42 +0700

    add department

---
+++ components/page/department/DepartmentAccountSide.vue
@@ -0,0 +1,26 @@
+ <template>
+   <div class="department-side">
+       <div>
+           <div class="editavatar__Department">
+               <app-avatar src="https://picsum.photos/150/150" :size="125"/>
+               <div class="btn-camera">
+                   <IconCamera/>
+               </div>
+           </div>
+           <strong style="border:1px solid red;">Phòng GDĐT Huyện Mê Linh</strong>
+       </div>
+   </div>
+ </template>
+ 
+ <script>
+ import IconCamera from "~/assets/svg/icons/camera.svg?inline"
+ export default {
+     components:{
+         IconCamera
+     }
+ }
+ </script>
+ 
+ <style lang="scss">
+@import "~/assets/scss/components/department/_department-account-side.scss";
+ </style>
---
+++ pages/department/account.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+      <div class="col-3">
+          <DepartmentAccountSide/>
+      </div>
+      <div class="col-9">
+
+      </div>
+  </div>
+</template>
+
+<script>
+import DepartmentAccountSide from "~/components/page/department/DepartmentAccountSide"
+export default {
+    components:{
+        DepartmentAccountSide
+    }
+}
+</script>
+
+<style>
+
+</style>
