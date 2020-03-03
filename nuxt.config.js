f730e13619a264583e265f014199ceee0b3668c5
tiendatleo <tiendatleo218@gmail.com>
2020-03-03 09:57:34 +0700

    add vue-infinite-loading plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,7 @@ module.exports = {
         { src: "@/plugins/filters.js", ssr: false },
         { src: "@/plugins/vue-fragment.js", ssr: false },
         { src: "@/plugins/vue-toast.js", ssr: false },
+        { src: "@/plugins/vue-infinite-loading.js", ssr: false },
         // { src: "@/plugins/tooltip.js", ssr: false },
     ],
     /**
