ee83169c10e8d3b520e48a99ef293b70fc5394c7
ngduy <duy.nv.96@gmail.com>
2020-03-04 18:00:48 +0700

    Add header for page exercise, course

---
+++ layouts/exercise.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-wrap layout-default">
+    <TheHeaderExercise />
+
+    <nuxt class="page-content"/>
+
+    <portal-target name="modal" slim></portal-target>
+  </div>
+</template>
+
+<script>
+import TheHeaderExercise from "~/components/layout/header/HeaderExercise"
+
+export default {
+  components: {
+    TheHeaderExercise,
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/layouts/_default.scss";
+</style>
