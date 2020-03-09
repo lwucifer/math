f1ac20fcf8b776ad20b39e2377d80800c870003d
ngduy <duy.nv.96@gmail.com>
2020-03-09 14:07:34 +0700

    Change layout page create course

---
+++ components/page/course/create/CreateAction.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header-create">
+    <div class="header-create__content container">
+      <app-button class="header-create__btn header-create__btn-back" outline square>
+        <IconArrowLeft class="icon mr-2" />
+        Quay lại
+      </app-button>
+
+      <div class="header-create__right">
+        <app-button class="header-create__btn mr-4" square>Lưu</app-button>
+        <app-button class="header-create__btn" color="secondary" square>Xoá</app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import IconArrowLeft from '~/assets/svg/design-icons/arrow-left.svg?inline';
+
+  export default {
+    components: {
+      IconArrowLeft
+    }
+  }
+</script>
+
+<style scoped>
+  @import "~/assets/scss/components/header-create/_header-create.scss";
+</style>
