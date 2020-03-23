c9ddf0bdaccb5212901d3bac65aa9d60c4631361
tiendatleo <tiendatleo218@gmail.com>
2020-03-23 18:51:58 +0700

    update page elearing detail

--- pages/elearning/_id/index.vue
+++ pages/elearning/_id/index.vue
@@ -6,8 +6,11 @@
 
         <div class="elearning-view__info">
           <div class="author">
-            <app-avatar :src="info.teacher.avatar.low" :size="32" />
-            <span class="name ml-2">{{ info.teacher.name }}</span>
+            <app-avatar
+              :src="info.teacher && info.teacher.avatar ? info.teacher.avatar.low : null"
+              :size="32"
+            />
+            <span class="name ml-2">{{ info.teacher ? info.teacher.name : '' }}</span>
           </div>
 
           <div class="views ml-auto">
@@ -17,75 +20,164 @@
 
           <div class="stars">
             <app-stars :stars="Math.floor(lesson.stars)" :size="16" />
-            <strong class="ml-3">{{ lesson.stars }}</strong>
-            <span>({{ lesson.rates }})</span>
+            <strong class="ml-3">{{ lesson.stars }}</strong>&nbsp;
+            <span class="text-sub">({{ lesson.rates }})</span>
           </div>
         </div>
 
         <div class="elearning-view__main-nav" v-sticky>
-          <a class="scroll-link" href="#introduce" @click.prevent="scrollTo('introduce')">Giới thiệu</a>
+          <a
+            class="scroll-link"
+            href="#introduce"
+            @click.prevent="scrollTo('#introduce')"
+          >Giới thiệu</a>
           <a
             class="scroll-link"
             href="#course-content"
-            @click.prevent="scrollTo('course-content')"
-          >Nội dung bài giảng</a>
-          <a class="scroll-link" href="#teacher" @click.prevent="scrollTo('teacher')">Giáo viên</a>
-          <a class="scroll-link" href="#review" @click.prevent="scrollTo('review')">Đánh giá</a>
+            @click.prevent="scrollTo('#course-content')"
+          >Nội dung {{ typeText }}</a>
+          <a class="scroll-link" href="#teacher" @click.prevent="scrollTo('#teacher')">Giáo viên</a>
+          <a class="scroll-link" href="#review" @click.prevent="scrollTo('#review')">Đánh giá</a>
         </div>
 
         <section class="scroll-target" id="introduce">
-          <div class="box">
-            <h5 class="mb-4">Lợi ích từ bài giảng</h5>
-            <div>{{ info.benefits }}</div>
+          <div class="box mb-4">
+            <h5 class="mb-4">Lợi ích từ {{ typeText }}</h5>
+
+            <div v-if="typeof info.benefits === 'string'" class="d-flex">
+              <template v-if="info.benefits">
+                <IconCheckCircle class="icon body-1 mr-2" />
+                <p v-html="info.benefits" />
+              </template>
+            </div>
+
+            <span v-else-if="!info.benefits" class="caption text-sub">Chưa có nội dung</span>
+
+            <div v-else class="row">
+              <div
+                v-for="(item, index) in info.benefits"
+                :key="index"
+                class="col-md-6 d-flex mb-15"
+              >
+                <IconCheckCircle class="icon body-1 mr-2" />
+                <p v-html="item" />
+              </div>
+            </div>
           </div>
 
-          <div class="box">
+          <div class="box mb-4">
             <h5 class="mb-4">Mô tả tổng quát</h5>
-            <div>
-              <p>- Bạn đang mong muốn xây dựng một Hệ thống Kinh doanh Online Bài Bản cho riêng mình mà vẫn chưa tìm được hướng đi rõ ràng từ việc xác định sản phẩm kinh doanh - mô hình kinh doanh phù hợp, cách để liên hệ nhà cung cấp để đàm phán nhập hàng, cách nghiên cứu khách hàng, đối thủ, quảng cáo và tối ưu...... ?</p>
-              <p>- Bạn đã có kinh nghiệm Kinh doanh online, và đang mong muốn mở rộng Hệ Thống Online Đa Kênh ngoài kênh truyền thống sang Facebook, Instagram, Zalo, Youtube, Email Marketing, Website, SMS...?</p>
-            </div>
-            <div class="text-center mt-3 mb-3">
+            <div v-if="info.description" v-html="info.description"></div>
+            <span v-else class="caption text-sub">Chưa có nội dung.</span>
+            <!-- <div class="text-center my-3">
               <a class="btn-load-more">Xem thêm</a>
-            </div>
+            </div>-->
           </div>
         </section>
 
         <section class="box elearning-view__content scroll-target" id="course-content">
-          <h5>Nội dung bài giảng</h5>
-          <div class="info">
-            <div class="info-item">
-              Thể loại:
-              <strong class="color-primary">Marketing</strong>
-            </div>
-            <div class="info-item">
+          <h5>Nội dung {{ typeText }}</h5>
+          <div class="row flex-wrap info">
+            <div class="col-auto">
               Trình độ:
-              <strong class="color-primary">Lớp 10</strong>
+              <strong class="color-primary">{{ levelText }}</strong>
             </div>
-            <div class="info-item">
+            <div class="col-auto">
               Môn học:
-              <strong class="color-primary">Toán</strong>
+              <strong class="color-primary">{{ subjectText }}</strong>
             </div>
-            <div class="info-item">
+            <div class="col-auto">
               Số bài giảng:
-              <strong class="color-primary">1</strong>
+              <strong class="color-primary">chưa ghép</strong>
             </div>
-            <div class="info-item">
+            <div class="col-auto">
               Thời lượng:
-              <strong class="color-primary">23:50</strong>
+              <strong class="color-primary">{{ info.duration || '--' }}</strong>
             </div>
           </div>
 
-          <div class="content-item">
-            <IconPlayO class="mr-3" />Bài 1: Tạo và tối ưu tài khoản Instagram trên máy tính
-            <span class="ml-auto">23:50</span>
+          <div class="evlc-item mb-3">
+            <div class="evlc-item__head">
+              <div class="evlc-item__head-left mr-4">Bài giảng đại số lớp 10</div>
+              <div class="evlc-item__head-right ml-auto">
+                <button class="evlc-item__btn evlc-item__btn-collapse active">
+                  <IconAngleDown class="icon" />
+                </button>
+              </div>
+            </div>
+
+            <div class="evlc-item__body bg-input-gray">
+              <div class="d-flex align-items-center text-error py-3">
+                <IconFileAlt class="icon subheading mr-2" />Bài tập số 1
+              </div>
+              <app-divider class="my-0" />
+              <div class="d-flex align-items-center text-error py-3">
+                <IconFileAlt class="icon subheading mr-2" />Bài tập số 2
+              </div>
+            </div>
           </div>
-          <div class="content-item">
+
+          <div class="evlc-item mb-3">
+            <div class="evlc-item__head">
+              <div class="evlc-item__head-left mr-4">
+                <b>Chương 1:</b> Tạo và tối ưu tài khoản Instagram trên máy tính
+              </div>
+              <div class="evlc-item__head-right d-flex align-items-center ml-auto">
+                <span class="mr-3">3 Bài</span>
+                <button class="evlc-item__btn evlc-item__btn-collapse active">
+                  <IconAngleDown class="icon" />
+                </button>
+              </div>
+            </div>
+
+            <div class="evlc-item__body bg-input-gray">
+              <div class="d-flex align-items-center py-3">
+                <div class="d-flex align-items-center mr-4">
+                  <IconPlayCircle class="icon subheading mr-2 text-primary" />Bài 1: Tạo và tối ưu tài khoản instagran trên máy tính
+                </div>
+                <div class="ml-auto">10:15</div>
+              </div>
+              <app-divider class="my-0" />
+              <div class="d-flex align-items-center py-3">
+                <div class="d-flex align-items-center mr-4">
+                  <IconPlayCircle class="icon subheading mr-2 text-primary" />Bài 2: Tạo và tối ưu tài khoản instagran trên máy tính
+                </div>
+                <div class="ml-auto">10:15</div>
+              </div>
+              <app-divider class="my-0" />
+              <div class="d-flex align-items-center py-3">
+                <div class="d-flex align-items-center mr-4">
+                  <IconPlayCircle class="icon subheading mr-2 text-primary" />Bài 3: Tạo và tối ưu tài khoản instagran trên máy tính
+                </div>
+                <div class="ml-auto">10:15</div>
+              </div>
+              <app-divider class="my-0" />
+            </div>
+          </div>
+
+          <div class="evlc-item mb-3">
+            <div class="evlc-item__head">
+              <div class="evlc-item__head-left mr-4">
+                <b>Chương 2:</b> Tạo và tối ưu tài khoản Instagram trên máy tính
+              </div>
+              <div class="evlc-item__head-right d-flex align-items-center ml-auto">
+                <span class="mr-3">3 Bài</span>
+                <button class="evlc-item__btn evlc-item__btn-collapse">
+                  <IconAngleDown class="icon" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="download-item">
             <IconBooks class="fill-primary mr-3" />Tài liệu đính kèm
-            <strong class="ml-auto color-primary d-flex-center">
+            <a
+              href
+              class="ml-auto color-primary d-flex-center font-weight-bold text-decoration-none"
+            >
               Tải về
               <IconDownload class="ml-2" />
-            </strong>
+            </a>
           </div>
         </section>
 
@@ -98,7 +190,7 @@
           <hr />
 
           <section class="scroll-target pt-2" id="review">
-            <h5 class="mt-3 mb-4">Đánh giá bài giảng</h5>
+            <h5 class="mt-3 mb-4">Đánh giá {{ typeText }}</h5>
             <ElearningRates />
           </section>
         </div>
@@ -126,8 +218,11 @@
 <script>
 import { mapState } from "vuex";
 import * as actionTypes from "~/utils/action-types";
+import { ELEARNING_TYPES } from "~/utils/constants";
 
 import InfoService from "~/services/elearning/public/Info";
+import LevelService from "~/services/elearning/public/Level";
+import SubjectService from "~/services/elearning/public/Subject";
 
 import CourseTeacherInfo from "~/components/page/course/CourseTeacherInfo";
 import ElearningSliderTab from "~/components/page/elearning/ElearningSliderTab";
@@ -138,6 +233,13 @@ import IconEye from "~/assets/svg/icons/eye.svg?inline";
 import IconPlayO from "~/assets/svg/icons/play-o.svg?inline";
 import IconDownload from "~/assets/svg/icons/download.svg?inline";
 import IconBooks from "~/assets/svg/icons/books.svg?inline";
+import IconAngleDown from "~/assets/svg/design-icons/angle-down.svg?inline";
+const IconCheckCircle = () =>
+  import("~/assets/svg/icons/check-circle.svg?inline");
+const IconFileAlt = () =>
+  import("~/assets/svg/design-icons/file-alt.svg?inline");
+const IconPlayCircle = () =>
+  import("~/assets/svg/design-icons/play-circle.svg?inline");
 
 export default {
   name: "E-learningDetail",
@@ -150,18 +252,34 @@ export default {
     IconEye,
     IconPlayO,
     IconBooks,
-    IconDownload
+    IconDownload,
+    IconCheckCircle,
+    IconAngleDown,
+    IconFileAlt,
+    IconPlayCircle
   },
 
   async asyncData({ $axios, params }) {
-    const getInfo = await new InfoService($axios)[actionTypes.BASE.LIST]({
-      params: {
-        elearning_id: params.id
-      }
-    });
+    const getInfo = () =>
+      new InfoService($axios)[actionTypes.BASE.LIST]({
+        params: {
+          elearning_id: params.id
+        }
+      });
+    const getLevels = () => new LevelService($axios)[actionTypes.BASE.LIST]();
+    const getSubjects = () =>
+      new SubjectService($axios)[actionTypes.BASE.LIST]();
+
+    const [
+      dataInfo = {},
+      dataLevels = {},
+      dataSubjects = {}
+    ] = await Promise.all([getInfo(), getLevels(), getSubjects()]);
 
     return {
-      info: getInfo.data || {}
+      info: dataInfo.data || {},
+      levels: dataLevels.data || [],
+      subjects: dataSubjects.data || []
     };
   },
 
@@ -283,11 +401,45 @@ export default {
   },
 
   computed: {
-    ...mapState("auth", ["loggedUser"])
+    ...mapState("auth", ["loggedUser"]),
+
+    typeText() {
+      if (this.info) {
+        switch (this.info.type) {
+          case ELEARNING_TYPES.LECTURE:
+            return "bài giảng";
+            break;
+          case ELEARNING_TYPES.COURSE:
+            return "khoá học";
+            break;
+          default:
+            break;
+        }
+      }
+    },
+
+    levelText() {
+      const [level = {}] = this.levels.filter(
+        level => this.info.level === level.id
+      );
+      return level.name || "";
+    },
+
+    subjectText() {
+      const [subject = {}] = this.subjects.filter(
+        subject => this.info.subject === subject.code
+      );
+      return subject.name || "";
+    }
   },
 
   mounted() {
     window.addEventListener("scroll", this.bindScrollStatus);
+
+    // if (this.$route.hash && process.browser) {
+    //   const hashEl = document.querySelector(this.$route.hash);
+    //   hashEl && this.scrollTo(this.$route.hash);
+    // }
   },
 
   beforeDestroy() {
@@ -304,7 +456,7 @@ export default {
       for (const el of target) {
         const react = el.getBoundingClientRect();
 
-        if (window.scrollY + react.top <= scrollDistance) {
+        if (window.scrollY + react.top <= scrollDistance + 1) {
           Array.from(link).forEach(linkEl => {
             const activeLink = document.querySelector(
               `.scroll-link[href="#${el.id}"]`
@@ -317,10 +469,11 @@ export default {
     },
 
     scrollTo(id) {
-      const target = document.getElementById(id);
+      const target = document.querySelector(id);
+      const navLink = document.querySelector(".elearning-view__main-nav");
       const targetTop = target.getBoundingClientRect().top;
       const scrollPosition = targetTop + window.scrollY;
-      const navLink = document.querySelector(".elearning-view__main-nav");
+      const hash = this.$route.hash;
       // this.requestFrameId && window.cancelAnimationFrame(this.requestFrameId);
       // this.requestFrameId = window.requestAnimationFrame(() => {
       window.scrollTo({
@@ -329,6 +482,10 @@ export default {
         behavior: "smooth"
       });
       // })
+      // this.$router.replace({
+      //   path: this.$route.path,
+      //   hash: id
+      // });
     }
   }
 };
