0abff43ad0a07ef67cfa4496c84453f5d7faa573
tiendatleo <tiendatleo218@gmail.com>
2020-03-25 09:19:56 +0700

    update elearing detail

--- components/page/elearning/ElearningReview.vue
+++ components/page/elearning/ElearningReview.vue
@@ -4,40 +4,49 @@
       <div class="row items-center">
         <div class="col-md-6 col-sm-12">
           <div class="text-center d-inline-block">
-            <strong class="h1 color-primary">4.5</strong>
-            <app-stars :stars="4" :size="16" class="mt-2 mb-3" />
-            <p class="color-999">(15 người đánh giá)</p>
+            <strong class="h1 color-primary">{{ get(reviews, 'reviews.averageRate', 0) }}</strong>
+            <app-stars :stars="Math.floor(get(reviews, 'reviews.averageRate', 0))" :size="16" class="mt-2 mb-3" />
+            <p class="color-999">({{ get(reviews, 'reviews.totalReview', 0) }} người đánh giá)</p>
           </div>
         </div>
         <div class="col-md-6 col-sm-12">
-          <ElearningStars :five="50" :four="30" :three="10" :two="10" :one="0" />
+          <ElearningStars :rates="get(reviews, 'reviews.rates', [])" />
         </div>
       </div>
     </div>
 
     <div class="elearning-review__nav mb-4">
       <app-button
-        v-for="tab in tabs"
         class="mr-4"
-        :color="tabActive === tab.key ? 'primary' : 'gray'"
+        :color="tabActive === 'all' ? 'primary' : 'gray'"
         normal
-        :key="tab.key"
         size="sm"
         square
-        @click="tabActive = tab.key"
+        @click="tabActive = 'all'"
+      >Tất cả</app-button>
+
+      <app-button
+        v-for="rate in get(reviews, 'reviews.rates', [])"
+        class="mr-4"
+        :color="tabActive === rate.rate ? 'primary' : 'gray'"
+        normal
+        :key="rate.rate"
+        size="sm"
+        square
+        @click="tabActive = rate.rate"
       >
-        {{ tab.text }}
-        <template v-if="tab.quantity">({{ tab.quantity || 0 }})</template>
+        {{ `${rate.rate} sao (${rate.count})` }}
       </app-button>
     </div>
 
     <div class="elearning-review__commnents">
-      <ElearningReviewComment :data="{}" v-for="(item, index) in  5" :key="index" />
+      <ElearningReviewComment :data="item" v-for="item in reviews.content || []" :key="item.id" />
       <app-pagination :pagination="pagination" @pagechange="onPageChange" class="mt-4 mb-3" />
     </div>
   </div>
 </template>
 <script>
+import { get } from "lodash";
 import ElearningStars from "~/components/page/elearning/ElearningStars";
 import ElearningReviewComment from "~/components/page/elearning/ElearningReviewComment";
 
@@ -61,19 +70,13 @@ export default {
         page: 6,
         pager: 10
       },
-      tabActive: "all",
-      tabs: [
-        { key: "all", text: "Tất cả", quantity: "" },
-        { key: "5", text: "5 sao", quantity: "" },
-        { key: "4", text: "4 sao", quantity: "" },
-        { key: "3", text: "3 sao", quantity: "" },
-        { key: "2", text: "2 sao", quantity: "" },
-        { key: "1", text: "1 sao", quantity: "" }
-      ]
+      tabActive: "all"
     };
   },
 
   methods: {
+    get,
+
     onPageChange(e) {
       const that = this;
       that.pagination = { ...that.pagination, ...e };
--- components/page/elearning/ElearningReviewComment.vue
+++ components/page/elearning/ElearningReviewComment.vue
@@ -2,22 +2,23 @@
   <div class="elearning-review-comment">
     <div class="elearning-review-comment__top d-flex-center">
       <div class="elearning-review-comment__info d-flex-center mr-5">
-        <app-avatar src="https://picsum.photos/40/40" :size="40" />
-        <strong class="ml-3">Tomothy Webb</strong>
+        <app-avatar :src="data && data.avatar || 'https://picsum.photos/40/40'" :size="40" />
+        <strong class="ml-3">{{ data.fullname }}</strong>
       </div>
 
       <div class="elearning-review-comment__right ml-auto">
         <div class="d-flex-center color-666">
-          <IconClock width="14" height="14" class="mr-2" />16:48 20/08/2019
+          <IconClock width="14" height="14" class="mr-2" /> {{ [ data.created, DATETIME_RECEIVE] | moment(DATETIME_DISPLAY) }}
         </div>
       </div>
     </div>
 
     <app-stars :stars="4" :size="16" class="mt-4 mb-3" />
-    <p class="color-666">Bài giảng rất hay</p>
+    <p class="color-666">{{ data.comment }}</p>
   </div>
 </template>
 <script>
+import { DATETIME_DISPLAY, DATETIME_RECEIVE } from "~/utils/config";
 import IconClock from "~/assets/svg/icons/clock.svg?inline";
 
 export default {
@@ -25,10 +26,27 @@ export default {
     IconClock
   },
 
+  data() {
+    return {
+      DATETIME_DISPLAY,
+      DATETIME_RECEIVE
+    }
+  },
+
   props: {
     data: {
       type: Object,
-      default: () => {}
+      default: () => ({}),
+      validator: value =>
+        [
+          "id",
+          "avatar",
+          "fullname",
+          "username",
+          "comment",
+          "rate",
+          "created"
+        ].every(key => key in value)
     }
   }
 };
