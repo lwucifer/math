8d73a39dbb58d5c220bdda34fc0a7ee43a47bf8d
ngduy <duy.nv.96@gmail.com>
2020-03-27 18:06:07 +0700

    Add file size filter

--- plugins/filters.js
+++ plugins/filters.js
@@ -5,13 +5,46 @@ import Vue from "vue"
  * @param {number} num
  * @param {string} separator
  */
-function toThousandFilter(num, separator = '.') {
+export function toThousandFilter(num, separator = '.') {
     // console.log("[toThousandFilter]", num)
     return (+num || 0).toString().replace(/^-?\d+/g, m => m.replace(/(?=(?!\b)(\d{3})+$)/g, separator))
 }
 
+/**
+ * Filesize Filter
+ * @Param {number} size
+ * @return string
+ */
+export function fileSizeFilter(size) {
+    if (isNaN(size))
+        size = 0;
+
+    if (size < 1024)
+        return size + ' Bytes';
+
+    size /= 1024;
+
+    if (size < 1024)
+        return size.toFixed(2) + ' KB';
+
+    size /= 1024;
+
+    if (size < 1024)
+        return size.toFixed(2) + ' MB';
+
+    size /= 1024;
+
+    if (size < 1024)
+        return size.toFixed(2) + ' GB';
+
+    size /= 1024;
+
+    return size.toFixed(2) + ' TB';
+}
+
 const filters = {
     toThousandFilter,
+    fileSizeFilter
 }
 
 // register global utility filters
