b1059d99633b3e1cddf134c9c0e20b95be528c93
tiendatleo <tiendatleo218@gmail.com>
2020-04-14 18:08:54 +0700

    fix display content index

--- pages/index.vue
+++ pages/index.vue
@@ -183,7 +183,7 @@
 
         <div class="col-md-4">
           <AsideBox :title="`Tin nhắn`" link="/messages" linkText="Xem toàn bộ >>">
-            <app-content-box
+            <!-- <app-content-box
               v-for="message in messagesConverted"
               v-bind="message"
               class="mb-4"
@@ -191,7 +191,7 @@
               size="sm"
               :key="message.id"
               :to="`/messages/t/${message.id}`"
-            />
+            /> -->
           </AsideBox>
 
           <AsideBox title="Khóa học Online nổi bật">
@@ -308,8 +308,8 @@ export default {
 
   async asyncData({ $axios }) {
     const getFeeds = () => new FeedsService($axios)[actionTypes.BASE.LIST]();
-    const getMessages = () =>
-      new LimitMessagesSerice($axios)[actionTypes.BASE.LIST]();
+    // const getMessages = () =>
+    //   new LimitMessagesSerice($axios)[actionTypes.BASE.LIST]();
     const getFreeCourse = () =>
       new SearchService($axios)[actionTypes.BASE.ADD]({
         free: true,
@@ -323,19 +323,19 @@ export default {
 
     const [
       { data: feeds = {} },
-      { data: messages = [] },
+      // { data: messages = [] },
       { data: freeCourses = [] },
       { data: privateCourses = [] }
     ] = await Promise.all([
       getFeeds(),
-      getMessages(),
+      // getMessages(),
       getFreeCourse(),
       getPrivateCourse()
     ]);
 
     return {
       feeds: feeds || [],
-      messages: messages || [],
+      // messages: messages || [],
       freeCourses: freeCourses.content || [],
       privateCourses: privateCourses.content || []
     };
@@ -387,19 +387,19 @@ export default {
       return "id" in store.state.auth.token ? store.state.auth.token.id : null;
     },
 
-    messagesConverted() {
-      return this.messages ? this.messages.map(item => {
-        return {
-          id: item.room.id,
-          title: item.room.members
-            .filter(member => member.user_id !== this.userId)
-            .map(member => member.fullname)
-            .join(", "),
-          desc: item.message.content,
-          image: get(item, 'room.room_avatar.low', null)
-        };
-      }) : [];
-    }
+    // messagesConverted() {
+    //   return this.messages && this.messsages.length ? this.messages.map(item => {
+    //     return {
+    //       id: item.room.id,
+    //       title: item.room.members
+    //         .filter(member => member.user_id !== this.userId)
+    //         .map(member => member.fullname)
+    //         .join(", "),
+    //       desc: item.message.content,
+    //       image: get(item, 'room.room_avatar.low', null)
+    //     };
+    //   }) : [];
+    // }
   },
 
   mounted() {
@@ -738,7 +738,7 @@ export default {
         actionTypes.BASE.LIST
       ]({
         params: {
-          page: this.feeds.page.number + 1
+          page: get(this, 'feeds.page.number', 1) + 1
         }
       });
 
