d62360e24981771b32df9e2dc739e55b41bf24ab
Your Name <you@example.com>
2020-04-15 11:31:27 +0700

    fix reload page

--- pages/elearning/manager/courses/create/index.vue
+++ pages/elearning/manager/courses/create/index.vue
@@ -66,13 +66,6 @@ export default {
     window.removeEventListener("beforeunload", this.preventNav);
   },
 
-  beforeRouteLeave(to, from, next) {
-    if (!window.confirm("Leave without saving?")) {
-      return;
-    }
-    next();
-  },
-
   methods: {
     preventNav(event) {
       event.preventDefault();
