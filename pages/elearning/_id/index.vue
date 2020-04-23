706afdbd58b990c209d2eb649f7b28c04c78b463
Your Name <you@example.com>
2020-04-23 09:40:34 +0700

    course teacher info

--- pages/elearning/_id/index.vue
+++ pages/elearning/_id/index.vue
@@ -13,13 +13,10 @@
         <ElearningContent :program="program" :info="info" />
 
         <div class="box">
-          <section class="scroll-target" id="teacher">
-            <h5 class="mb-4">Thông tin giáo viên</h5>
-            <CourseTeacherInfo
-              :teacher_id="get(info, 'teacher.id', '')"
-              class="mb-3"
-            />
-          </section>
+          <CourseTeacherInfo
+            :teacher_id="get(info, 'teacher.id', '')"
+            class="mb-3"
+          />
 
           <hr class="mt-3 mb-4" />
 
