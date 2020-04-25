c1b7a32da13b52b0832336cb411bb988feefd8b4
chungpd <nh0crio96@gmail.com>
2020-04-25 11:32:41 +0700

    update

--- pages/payment/cart.vue
+++ pages/payment/cart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row" v-if="true">
       <div class="col-md-8 bg-white py-3 px-4">
         <h6 class="text-primary">Giỏ hàng</h6>
         <div
@@ -15,6 +15,17 @@
         <CartSideRight />
       </div>
     </div>
+    <div class="bg-white py-3 px-4" v-else>
+        <div class="d-flex align-items-center mb-4">
+          <h4>Giỏ hàng </h4>
+          <span class="text-gray">(0 sản phẩm)</span>
+        </div>
+        <div class="text-center my-4">
+          <img src="~assets/images/payment/undraw_empty_xct9 (1) 1.png">
+          <p class="my-4">Không có sản phẩm nào trong giỏ hàng của bạn.</p>
+          <app-button square>Tiếp tục mua hàng</app-button>
+        </div>
+      </div>
   </div>
 </template>
 
