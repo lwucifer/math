d521cc488a3f8b9ca2528983e72b863242268ef7
trungnq@bcnex.net <trungnq@bcnex.net>
2020-04-29 17:54:58 +0700

    demo zoom on page ok

---
+++ layouts/empty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-wrap">
+    <nuxt class="page-content"/>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Empty"
+  }
+</script>
+
+<style lang="scss">
+  @import "~/assets/scss/layouts/_create.scss";
+</style>
