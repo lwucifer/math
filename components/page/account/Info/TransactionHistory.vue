8ce57f033a18fa0915c7cd5691252d34ce0809fa
ngduy <duy.nv.96@gmail.com>
2020-04-29 17:18:01 +0700

    modify transaction history component

---
+++ components/page/account/Info/TransactionHistory.vue
@@ -0,0 +1,186 @@
+<template>
+  <sub-block-section>
+    <template v-slot:title>
+      <filter-form />
+    </template>
+    
+    <template v-slot:content>
+      <transaction-list
+        :pagination="pagination"
+        :list.sync="list"
+        :loading="loading"
+        @changedPagination="updatePagination"
+      />
+    </template>
+  </sub-block-section>
+</template>
+
+<script>
+import SchoolAccountSide from "~/components/page/school/SchoolAccountSide";
+import UploadAvatar from "~/components/page/account/Info/UploadAvatar";
+import FilterForm from "~/components/page/account/forms/TransactionFilter"
+import TransactionList from "~/components/page/account/tables/TransactionList"
+import * as actionTypes from "~/utils/action-types";
+import { mapState, mapActions } from "vuex";
+import { get } from "lodash";
+import { getDateBirthDay, getDateFormat } from "~/utils/moment";
+// Import faked data
+
+export default {
+  components: {
+    TransactionList,
+    SchoolAccountSide,
+    UploadAvatar,
+    FilterForm,
+    TransactionList
+  },
+  data() {
+    return {
+      pagination: {
+        totalElements: 0,
+        last: false,
+        totalPages: 1,
+        size: 10,
+        number: 0,
+        first: true,
+        numberOfElements: 0
+      },
+      params: {
+        page: 1,
+        size: 10,
+        // elearning_id: "39fe1dd5-2df2-465f-8cf7-59d4ead68189"
+        elearning_id: null
+      },
+      list: [
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+        {
+          id: 1,
+          name: "Mua khóa học Đại số 10",
+          price: "2290000",
+          code: "D88HKDKD",
+          type: "Mua",
+          pay: "Thanh toán Online",
+          time: "16:50:30 20-11-2019"
+        },
+        {
+          id: 1,
+          name: "Bán khóa học Đại số 10",
+          price: "2990000",
+          code: "D88HKDKD",
+          type: "Bán",
+          pay: "Chuyển khoản",
+          time: "06:50:30 10-11-2019"
+        },
+      ],
+      loading: false
+    }
+  },
+  watch:{
+  
+  },
+  methods: {
+    updatePagination(val) {
+      // this.params.size !== val.size ? this.params.page = 1 : this.params.page = val.number + 1
+      // this.params.size = val.size
+      // this.getList()
+    },
+  },
+  computed: {
+  
+  },
+  created() {
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
--- pages/_id/info/index.vue
+++ pages/_id/info/index.vue
@@ -10,57 +10,55 @@
             title="Thông tin tài khoản"
           >
             <template v-slot:content>
-              <div class="">
-                <general-info />
-                <!--<sub-block-section-->
-                  <!--title="Thông tin cá nhân"-->
-                <!--&gt;-->
-                  <!--<template v-slot:content>-->
-                    <!--<div class="wrapInfoAccount">-->
-                      <!--<div class="d-flex">-->
-                        <!--<app-button-->
-                          <!--square-->
-                          <!--class="btnAccountLink_account-info"-->
-                          <!--v-if="!accountLink.list.linked"-->
-                          <!--v-on:click="accountLink.showModal=true"-->
-                        <!--&gt;-->
-                          <!--<IconPlus class="icon"/>-->
-                          <!--<span class="ml-3">Liên kết trường học</span>-->
-                        <!--</app-button>-->
-      <!---->
-                        <!--<AccountLinkModal-->
-                          <!--:visible="accountLink.showModal"-->
-                          <!--@click-close="closeLinkModal"-->
-                          <!--@handleRefresh="handleRefresh"-->
-                        <!--/>-->
+              <general-info />
+              <!--<sub-block-section-->
+                <!--title="Thông tin cá nhân"-->
+              <!--&gt;-->
+                <!--<template v-slot:content>-->
+                  <!--<div class="wrapInfoAccount">-->
+                    <!--<div class="d-flex">-->
+                      <!--<app-button-->
+                        <!--square-->
+                        <!--class="btnAccountLink_account-info"-->
+                        <!--v-if="!accountLink.list.linked"-->
+                        <!--v-on:click="accountLink.showModal=true"-->
+                      <!--&gt;-->
+                        <!--<IconPlus class="icon"/>-->
+                        <!--<span class="ml-3">Liên kết trường học</span>-->
+                      <!--</app-button>-->
     <!---->
-                      <!--</div>-->
-                      <!--<app-input labelFixed v-model="name" label="Họ và tên" disabled />-->
-                      <!--<app-input v-model="phone" label="Số điện thoại" disabled />-->
-                      <!--<app-input v-model="email" label="Email" disabled/>-->
-                      <!--<div class="picker-group__profileInfo">-->
-                        <!--<app-input v-model="sex" label="Giới tính" class="sex_Profile" disabled/>-->
-                        <!--<app-input v-model="birthday" label="Ngày sinh" class="birthday_Profile" disabled/>-->
-                      <!--</div>-->
+                      <!--<AccountLinkModal-->
+                        <!--:visible="accountLink.showModal"-->
+                        <!--@click-close="closeLinkModal"-->
+                        <!--@handleRefresh="handleRefresh"-->
+                      <!--/>-->
+  <!---->
                     <!--</div>-->
-                    <!--<app-button color="red" square v-on:click="showChangePass=true" class="btnChangePassword">-->
-                      <!--<span class="ml-3">Thay đổi mật khẩu</span>-->
-                    <!--</app-button>-->
-                    <!--<AccountChangePasswordModal-->
-                      <!--:visible="showChangePass"-->
-                      <!--@click-close="showChangePass = false"-->
-                    <!--/>-->
-                    <!--<AccountInfoStudent :profileList="profileInfo" v-if="accountLink.list.linked"/>-->
-                    <!--<div class="d-flex mt-4">-->
-                      <!--<app-button class="ml-auto" square>Lưu thay đổi</app-button>-->
+                    <!--<app-input labelFixed v-model="name" label="Họ và tên" disabled />-->
+                    <!--<app-input v-model="phone" label="Số điện thoại" disabled />-->
+                    <!--<app-input v-model="email" label="Email" disabled/>-->
+                    <!--<div class="picker-group__profileInfo">-->
+                      <!--<app-input v-model="sex" label="Giới tính" class="sex_Profile" disabled/>-->
+                      <!--<app-input v-model="birthday" label="Ngày sinh" class="birthday_Profile" disabled/>-->
                     <!--</div>-->
-                  <!--</template>-->
-                <!--</sub-block-section>-->
-                
-                <school-info />
+                  <!--</div>-->
+                  <!--<app-button color="red" square v-on:click="showChangePass=true" class="btnChangePassword">-->
+                    <!--<span class="ml-3">Thay đổi mật khẩu</span>-->
+                  <!--</app-button>-->
+                  <!--<AccountChangePasswordModal-->
+                    <!--:visible="showChangePass"-->
+                    <!--@click-close="showChangePass = false"-->
+                  <!--/>-->
+                  <!--<AccountInfoStudent :profileList="profileInfo" v-if="accountLink.list.linked"/>-->
+                  <!--<div class="d-flex mt-4">-->
+                    <!--<app-button class="ml-auto" square>Lưu thay đổi</app-button>-->
+                  <!--</div>-->
+                <!--</template>-->
+              <!--</sub-block-section>-->
+              
+              <school-info />
 
-                <department-info />
-              </div>
+              <department-info />
             </template>
           </block-section>
         </div>
