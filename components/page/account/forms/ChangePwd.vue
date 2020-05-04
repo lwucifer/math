a240b23493b4fc44a393e9db9e7933047d038b88
ngduy <duy.nv.96@gmail.com>
2020-05-04 16:35:06 +0700

    update change pwd page

--- components/page/account/forms/ChangePwd.vue
+++ components/page/account/forms/ChangePwd.vue
@@ -1,29 +1,181 @@
 <template>
-  <div>
-    Change pwd
-  </div>
+  <sub-block-section
+    :has-title="false"
+  >
+    <template v-slot:content>
+      <div class="form--normal">
+        <div class="row">
+          <div class="col-md-3"><label for="" class="form--normal__title">Mật khẩu cũ</label></div>
+          <div class="col-md-9">
+            <app-input
+              v-model="oldPassword"
+              type="password"
+              :validate="validateProps.oldPassword"
+              :error="$v.oldPassword.$invalid || validate.oldPassword"
+              :message="errorMessage.oldPassword"
+              autocomplete="new-password"
+              @input="handleOldPassword"
+              placeholder="Nhập mât khẩu cũ"
+            />
+          </div>
+          <div class="col-md-3"><label for="" class="form--normal__title">Mật khẩu mới</label></div>
+          <div class="col-md-9">
+            <app-input
+              v-model="newPassword"
+              type="password"
+              :error="$v.newPassword.$invalid || validate.newPassword"
+              :message="errorMessage.newPassword"
+              :validate="validateProps.newPassword"
+              @input="handleNewPassword"
+            />
+          </div>
+          <div class="col-md-3"><label for="" class="form--normal__title">Xác nhận mật khẩu</label></div>
+          <div class="col-md-9">
+            <app-input
+              v-model="coNewPassword"
+              type="password"
+              :error="$v.coNewPassword.$invalid || validate.coNewPassword"
+              :message="errorMessage.coNewPassword"
+              :validate="validateProps.coNewPassword"
+              @input="handleCoNewPassword"
+            />
+          </div>
+          <div class="col-md-3"></div>
+          <div class="col-md-9">
+            <app-button
+              @click="save"
+              size="lg"
+            >
+              Thay đổi mật khẩu
+            </app-button>
+          </div>
+        </div>
+      </div>
+    </template>
+  </sub-block-section>
 </template>
 
 <script>
-  import { DATE_SHORTCUT} from "~/utils/config";
+  import IconTick from "~/assets/svg/icons/tick.svg?inline";
+  import { mapState, mapActions } from "vuex";
+  import { ERRORS } from "~/utils/error-code";
+  import {
+    required,
+    minLength,
+    sameAs,
+    maxLength
+  } from "vuelidate/lib/validators";
+  import { validatePassword } from "~/utils/validations";
 
   export default {
     data() {
       return {
-        filters: {
-          time: ''
+        oldPassword: "",
+        newPassword: "",
+        coNewPassword: "",
+        error: false,
+        error2: false,
+        error3: false,
+        success: false,
+        errorMessage: {
+          oldPassword: "",
+          newPassword: "",
+          coNewPassword: ""
         },
-        DATE_SHORTCUT: DATE_SHORTCUT
+        validateProps: { oldPassword: "", newPassword: "", coNewPassword: "" },
+        validate: { oldPassword: true, newPassword: true }
+      };
+    },
+    validations: {
+      oldPassword: { required },
+      newPassword: { required },
+      coNewPassword: { required, sameAsPassword: sameAs("newPassword") }
+    },
+    computed: {
+      disabledBtn() {
+        const btnDisabled =
+          this.$v.$invalid ||
+          this.validate.oldPassword ||
+          this.validate.newPassword;
+        return btnDisabled;
       }
     },
     methods: {
-      submit() {
-        console.log('[Submit filter]')
+      ...mapActions("auth", ["changePassword"]),
+      save() {
+        let data = {
+          current_pwd: this.oldPassword,
+          new_pwd: this.newPassword,
+          verify_new_pass: this.coNewPassword
+        };
+        const doAdd = this.changePassword(data).then(result => {
+          if (result.success == 1) {
+            this.success = true;
+            // this.$emit("click-close");
+          } else {
+            this.$toasted.error(result.message)
+          }
+        });
+      },
+      handleOldPassword(_password) {
+        this.validate.oldPassword = true;
+        this.validateProps.oldPassword = "";
+        if (!this.$v.oldPassword.required) {
+          this.validateProps.oldPassword = 2;
+          this.errorMessage.oldPassword = "Trường này là bắt buộc";
+        } else if (validatePassword(_password)) {
+          this.validateProps.oldPassword = 1;
+          this.validate.oldPassword = false;
+        } else if (!validatePassword(_password)) {
+          this.validateProps.oldPassword = 2;
+          this.errorMessage.oldPassword =
+            "Mật khẩu phải có ít nhất 8 ký tự, bao gồm ít nhất 1 chữ cái in hoa, thường và 1 chữ số";
+        }
+      },
+      handleNewPassword(_password) {
+        this.validate.newPassword = true;
+        this.validateProps.newPassword = "";
+        if (!this.$v.newPassword.required) {
+          this.validateProps.newPassword = 2;
+          this.errorMessage.newPassword = "Trường này là bắt buộc";
+        } else if (validatePassword(_password)) {
+          this.validateProps.newPassword = 1;
+          this.validate.newPassword = false;
+        } else if (!validatePassword(_password)) {
+          this.validateProps.newPassword = 2;
+          this.errorMessage.newPassword =
+            "Mật khẩu phải có ít nhất 8 ký tự, bao gồm ít nhất 1 chữ cái in hoa, thường và 1 chữ số";
+        }
+      },
+      handleCoNewPassword() {
+        if (!this.$v.coNewPassword.required) {
+          this.validateProps.coNewPassword = 2;
+          this.errorMessage.coNewPassword = "Trường này là bắt buộc";
+        } else if (!this.$v.coNewPassword.sameAsPassword) {
+          this.validateProps.coNewPassword = 2;
+          this.errorMessage.coNewPassword = "Xác nhận mật khẩu không khớp";
+        } else {
+          this.validateProps.coNewPassword = 1;
+        }
+      },
+      reset() {
+        const that = this;
+        that.oldPassword = "";
+        that.newPassword = "";
+        that.coNewPassword = "";
+        that.error = false;
+        that.error2 = false;
+        that.error3 = false;
+        that.success = false;
+      }
+    },
+    watch: {
+      visible() {
+        this.reset();
       }
     }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss">
 </style>
---
+++ components/page/account/noti/UpdatePwd.vue
@@ -0,0 +1,46 @@
+<template>
+  <sub-block-section
+    :has-title="false"
+  >
+    <template v-slot:content>
+      <app-modal-block-noti-status
+        class="text-center mx-auto"
+        type="success"
+        title="Đổi mật khẩu thành công"
+        description="Tài khoản của bạn đã được cập nhật mật khẩu mới!"
+      >
+        <template v-slot:footer>
+          <app-button
+            size="xl"
+            nuxt
+            :to="'/' + accountInfo.id + '/info'"
+          >
+            Quay lại trang trước
+          </app-button>
+        </template>
+      </app-modal-block-noti-status>
+    </template>
+  </sub-block-section>
+</template>
+
+<script>
+import ImageAuth  from "~/components/page/auth/ImageAuth";
+import IconSuccess from '~/assets/svg/icons/success.svg?inline';
+import { getToken } from "~/utils/auth";
+
+export default {
+  components:{
+    ImageAuth,
+    IconSuccess
+  },
+  computed: {
+    accountInfo() {
+      return getToken()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/components/auth/_auth-v2.scss";
+</style>
