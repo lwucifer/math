4194de22fe62833e058cc1115511aeb76295e9ab
TrungNQ <quangtrung.uet@gmail.com>
2020-05-05 11:24:06 +0700

    fix mdw teacher role

--- middleware/teacher-role.js
+++ middleware/teacher-role.js
@@ -2,8 +2,10 @@ import { getToken } from "~/utils/auth";
 import { USER_ROLES } from "~/utils/constants";
 
 export default function ({ store, route, redirect }) {
-    const accountSigned = getToken();
-    const accountRole = accountSigned ? accountSigned.roles : [];
+    // const accountSigned = getToken();
+    const accountRole = store.getters['auth/roles'] || [];
+    console.log("[Middlware] teacher role accountRole", accountRole);
+    // const accountRole = accountSigned ? accountSigned.roles : [];
     const roleNames = accountRole.map(r => r.authority);
     const isTeacherRole = (roleNames.length > 0 && roleNames.includes(USER_ROLES.ROLE_TEACHER));
     console.log("[Middlware] teacher role", roleNames, isTeacherRole);
