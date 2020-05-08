4bdfc6434985c52c4622b74fb35a7c67caf5d4e2
Your Name <you@example.com>
2020-05-08 08:59:34 +0700

    program type lecture

---
+++ components/page/elearning/study/ElearningProgram.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="box11">
+    <p class="mb-3">
+      <strong>{{ get(progress, "total_lessons", 0) }} Bài giảng</strong>
+      ({{ get(progress, "duration", "") }})
+    </p>
+
+    <div v-if="progress.type === 'COURSE'">
+      <ElearningProgramCourse
+        v-for="(program, index) in get(progress, 'programs', [])"
+        :program="program"
+        :key="index"
+        :index="index"
+      />
+    </div>
+    <div v-else>
+      <ElearningProgramItem :lesson="get(progress, 'programs.0.lessons.0', null)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconPlay from "~/assets/svg/icons/play.svg?inline";
+import IconUpO from "~/assets/svg/icons/up-o.svg?inline";
+import IconDownO from "~/assets/svg/icons/down-o.svg?inline";
+import IconFileCheck from "~/assets/svg/design-icons/file-check.svg?inline";
+import IconFileEditAlt from "~/assets/svg/design-icons/file-edit-alt.svg?inline";
+import IconFileCheckAlt from "~/assets/svg/design-icons/file-check-alt.svg?inline";
+import IconFileClock from "~/assets/svg/icons/file-clock.svg?inline";
+import { get } from "lodash";
+import ElearningProgramItem from "~/components/page/elearning/study/ElearningProgramItem";
+import ElearningProgramCourse from "~/components/page/elearning/study/ElearningProgramCourse";
+
+export default {
+  components: {
+    IconPlay,
+    IconDownO,
+    IconUpO,
+    IconFileClock,
+    IconFileCheckAlt,
+    IconFileEditAlt,
+    IconFileCheck,
+    ElearningProgramItem,
+    ElearningProgramCourse,
+  },
+  props: {
+    progress: {},
+  },
+
+  created() {
+    console.log(this.progress);
+  },
+
+  updated() {
+    console.log(this.progress);
+  },
+
+  methods: { get },
+};
+</script>
