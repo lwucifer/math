0844e53a7c2abb2641f6c55194b589a314a14706
TrungNQ <quangtrung.uet@gmail.com>
2020-05-11 00:36:44 +0700

    update school

--- components/page/school/SchoolItem.vue
+++ components/page/school/SchoolItem.vue
@@ -22,11 +22,11 @@
       </div>
       <div class="school-item__info__statistic-item">
         <span><IconUser /></span>
-        <span class="value">{{ school.teacher_number }} giáo viên </span>
+        <span class="value">{{ get(school, 'teacher_number', '') }} giáo viên </span>
       </div>
       <div class="school-item__info__statistic-item">
         <span><IconUserGroup /></span>
-        <span class="value">{{ school.student_number }} học sinh </span>
+        <span class="value">{{ get(school, 'student_number', '') }} học sinh </span>
       </div>
     </div>
   </div>
