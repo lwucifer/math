3c5a82be01a1ea970455c6e4fb1363b974993465
trungnq@bcnex.net <trungnq@bcnex.net>
2020-05-15 11:53:51 +0700

    relogin after lienket truong hoc

--- components/page/auth/signin/SigninPhone.vue
+++ components/page/auth/signin/SigninPhone.vue
@@ -104,7 +104,7 @@ export default {
           if (result.success == true) {
             this.$emit("signin", true);
 
-            this.$router.push("/");
+            // this.$router.push("/");
           } else {
             this.showErrorWhenLogin(result);
           }
