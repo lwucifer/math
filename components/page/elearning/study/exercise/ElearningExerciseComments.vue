570c4cd2d7d419224596bdea225a4e64f39f2f5d
TrungNQ <quangtrung.uet@gmail.com>
2020-05-17 12:30:16 +0700

    view exercise comment

--- components/page/elearning/study/exercise/ElearningExerciseComments.vue
+++ components/page/elearning/study/exercise/ElearningExerciseComments.vue
@@ -2,7 +2,7 @@
   <div class="ee-comments">
     <div class="ee-comment-item">
       <div class="ee-comment-item__top">
-        <div class="ee-comment-item__heading">Bài tập 1</div>
+        <div class="ee-comment-item__heading">{{ result.name }}</div>
         <div class="ee-comment-item__point">
           Điểm:
           <b>{{ resultRate }}</b>
@@ -11,13 +11,13 @@
 
       <div class="ee-comment-item__comment">{{ result.note }}</div>
 
-      <div class="ee-comment-item__question-item" v-for="(s, index) in submission.length" :key="index">
+      <div class="ee-comment-item__question-item" v-for="(s, index) in mergeSubmissionQuestion" :key="index">
         <div class="ee-comment-item__title">Câu hỏi {{ s.index }}</div>
         <app-input
           textarea
           :autosize="false"
           readonly
-          :value="s.name"
+          :value="s.content"
         />
 
         <div class="ee-comment-item__title">Câu trả lời</div>
@@ -40,19 +40,35 @@ import { getExerciseResultText } from "~/plugins/filters";
 
 
 export default {
-  props: {
-    result: {
-      type: Object,
-      default: () => {}
-    },
-    submission: {
-      type: Array,
-      default: () => []
-    }
-  },
 
   computed: {
-    ...mapState("elearning/study/study-exercise", ["submissions", "questions"]),
+    ...mapState("elearning/study/study-exercise", ["submissions", "questions", "result"]),
+
+    mergeSubmissionQuestion() {
+      const tmp = this.questions.map((q, index) => {
+        const answered = this.submissions.find(s => s.question_id == q.id);
+        let correct_answer = null;
+        let student_answer = null;
+        let result = null;
+        let isUserTrue = false;
+        if (answered) {
+          correct_answer = q.answers.find(a => a.id == answered.correct_answer);
+          student_answer = answered.student_answer;
+          result = answered.result;
+          isUserTrue = answered.correct_answer == answered.student_answer;
+        }
+        return {
+          ...q,
+          // content: `${index + 1}.xxx${q.content}`,
+          correct_answer,
+          student_answer,
+          result,
+          isUserTrue
+        };
+      })
+      console.log("[mergeSubmissionQuestion]", tmp);
+      return tmp;
+    },
 
     resultRate() {
       return `${this.result.corrects}/${
