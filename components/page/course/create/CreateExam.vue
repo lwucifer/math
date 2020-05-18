476221db2aab9f56542d5aacdd2127775508d182
nguyenan <nguyenan1709@gmail.com>
2020-05-18 16:00:14 +0700

    update bai kiem tra

--- components/page/course/create/CreateExam.vue
+++ components/page/course/create/CreateExam.vue
@@ -8,7 +8,7 @@
 
       <div class="px-4">
         <app-alert type="info" class="mt-4" show-close>
-          Bạn có thể tạo bài tập cho khóa học của bạn tại đây. Nếu khóa học của
+          Bạn có thể tạo bài tập cho bài giảng/ khóa học của bạn tại đây. Nếu khóa học của
           bạn không yêu cầu làm bài tập, bạn có thể bỏ qua phần này và tiến hành
           gửi lên để được xét duyệt.
         </app-alert>
