14f879898c682ca8dabc1a256f8d86b014997455
nguyenan <nguyenan1709@gmail.com>
2020-05-20 15:06:26 +0700

    update

--- components/page/elearning/ElearningContentCourseItem.vue
+++ components/page/elearning/ElearningContentCourseItem.vue
@@ -3,8 +3,10 @@
         <div class="evlc-item__body bg-input-gray">
             <div class="d-flex align-items-center py-3">
             <div class="d-flex align-items-center mr-4">
-                <IconPlay class="icon subheading mr-2 fill-primary" />Bài
-                {{ lesson.index }}: {{ lesson.name }}
+                <IconLibraryBooks class="fill-primary mr-2" v-if="lesson.format == 'DOCX'"/>
+
+                <IconPlay v-else class="icon subheading mr-2 fill-primary" />
+                Bài {{ lesson.index }}: {{ lesson.name }}
             </div>
             
             <div class="ml-auto">{{ lesson.duration }}</div>
@@ -16,6 +18,7 @@
 
 <script>
 import IconPlay from '~/assets/svg/icons/play.svg?inline';
+import IconLibraryBooks from '~/assets/svg/icons/library-books.svg?inline';
 export default {
     props: {
         lesson: Object
@@ -23,6 +26,7 @@ export default {
 
     components: {
         IconPlay,
+        IconLibraryBooks
     }
 }
 </script>
