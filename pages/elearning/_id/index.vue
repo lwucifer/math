9c1205bbb0fba3fb0ba567a1c19fe130109bba02
Your Name <you@example.com>
2020-05-20 09:58:45 +0700

    share elearning facebook

--- pages/elearning/_id/index.vue
+++ pages/elearning/_id/index.vue
@@ -32,6 +32,14 @@
         />
       </div>
 
+      <div id="fb-root"></div>
+      <script
+        async
+        defer
+        crossorigin="anonymous"
+        src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v7.0"
+      ></script>
+
       <div class="col-md-4">
         <ElearningRightSide v-sticky sticky-offset="{ top: 90, bottom: 9 }" />
       </div>
@@ -77,7 +85,7 @@ const IconPlayCircle = () =>
 
 export default {
   name: "E-learningDetail",
-  
+
   components: {
     CourseTeacherInfo,
     ElearningSliderTab,
