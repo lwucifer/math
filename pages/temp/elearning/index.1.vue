8342f0825db9e719af439304806985bed3e9b63b
trungnq@bcnex.net <trungnq@bcnex.net>
2020-05-25 17:47:57 +0700

    revert home vs home elearning

--- pages/temp/elearning/index.1.vue
+++ pages/temp/elearning/index.1.vue
@@ -45,6 +45,138 @@
         </app-carousel>
       </ElearningHomeBox>
 
+      <!-- <ElearningHomeBox>
+        <h2 slot="title" class="heading-3 font-weight-medium mb-3">Dành cho bạn</h2>
+
+        <app-carousel :options="{ slidesPerView: 1, preventClicksPropagation: false }">
+          <template slot="default" slot-scope="{ classes }">
+            <div
+              v-for="item in forYou && forYou.content || []"
+              :key="item.elearning_id"
+              :class="classes"
+            >
+              <ElearningHomePersonalBox
+                :to="`/elearning/${item.elearning_id}`"
+                :image="get(item, 'avatar.medium', '')"
+                :livestream="item.is_streaming"
+                :name="item.name"
+                :teacher="item.teacher"
+                :averageRate="get(item, 'voting.rate', 0)"
+                :totalReview="get(item, 'voting.votes', 0)"
+                :price="item.price"
+                :originalPrice="item.original_price"
+                :free="!item.price"
+                :discount="item.discount"
+                :online-class="item.olclass_existed"
+                :description="`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor vitae maecenas nec nisl ornare nec duis. Tempor, non dolor tincidunt turpis eget sit egestas eget dui.`"
+              />
+            </div>
+          </template>
+        </app-carousel>
+      </ElearningHomeBox>
+
+      <ElearningHomeBox title="Môn học được yêu thích">
+        <div slot="title-right" class="swiper-pagination eh-subject-pagination"></div>
+
+        <app-carousel
+          class="eh-subject-carousel"
+          :options="{
+            slidesPerView: 5,
+            spaceBetween: 22,
+            preventClicksPropagation: false,
+            pagination: {
+              el: '.eh-subject-pagination',
+              type: 'bullets',
+              clickable: true
+            },
+          }"
+        >
+          <template slot-scope="{ classes }">
+            <div
+              v-for="item in votedSubjects && votedSubjects.content || []"
+              :key="item.id"
+              :class="classes"
+            >
+              <ElearningHomeSubjectItem
+                :title="item.name"
+                :to="`/elearning/search?subject=${item.code}&subject_name=${item.name}`"
+              >
+                <component slot="icon" :is="checkSubjectIcon(item.code)" />
+              </ElearningHomeSubjectItem>
+            </div>
+          </template>
+        </app-carousel>
+      </ElearningHomeBox>
+
+      <ElearningHomeBox>
+        <h2 slot="title" class="heading-3 font-weight-medium">Bài giảng mới nhất</h2>
+        <n-link slot="title-right" to class="link-readmore">
+          Xem thêm
+          <IconArrowForwardIos class="icon fill-opacity-1 ml-2" />
+        </n-link>
+
+        <app-carousel
+          :options="{ slidesPerView: 4, spaceBetween: 24, preventClicksPropagation: false }"
+        >
+          <template slot="default" slot-scope="{ classes }">
+            <div
+              v-for="item in newestLecture && newestLecture.content || []"
+              :key="item.id"
+              :class="classes"
+            >
+              <CourseItem2 class="my-0" :item="item" :size="'sm'" />
+            </div>
+          </template>
+        </app-carousel>
+      </ElearningHomeBox>
+
+      <ElearningHomeBox>
+        <h2 slot="title" class="heading-3 font-weight-medium">Khóa học mới nhất</h2>
+        <n-link slot="title-right" to class="link-readmore">
+          Xem thêm
+          <IconArrowForwardIos class="icon fill-opacity-1 ml-2" />
+        </n-link>
+
+        <app-carousel
+          :options="{ slidesPerView: 4, spaceBetween: 24, preventClicksPropagation: false }"
+        >
+          <template slot="default" slot-scope="{ classes }">
+            <div
+              v-for="item in newestCourse && newestCourse.content || []"
+              :key="item.id"
+              :class="classes"
+            >
+              <CourseItem2 class="my-0" :item="item" :size="'sm'" />
+            </div>
+          </template>
+        </app-carousel>
+      </ElearningHomeBox>
+
+      <ElearningHomeBox class="mb-4">
+        <h2 slot="title" class="heading-3 font-weight-medium mb-3">Giáo viên tiêu biểu</h2>
+
+        <app-carousel :options="{ slidesPerView: 3, spaceBetween: 35 }">
+          <template slot="default" slot-scope="{ classes }">
+            <div
+              v-for="item in highlightTeachers && highlightTeachers.content || []"
+              :key="item.teacher_id"
+              :class="classes"
+            >
+              <ElearningHomeTeacherCard
+                :to="`/public/profile/teacher?user_id=${get(item, 'teacher_id', '')}`"
+                :image="get(item, 'avatar.medium', '')"
+                :name="item.name"
+                school-name="Trường trung học cơ sở Nguyễn Trãi"
+                :num-of-lecture="item.lectures"
+                :num-of-course="item.courses"
+                :averageRate="item.voting_rate ? Math.floor(item.voting_rate) : 0"
+                :totalReview="item.votings"
+                :description="item.biography || 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor vitae maecenas nec nisl ornare nec duis. Tempor, non'"
+              />
+            </div>
+          </template>
+        </app-carousel>
+      </ElearningHomeBox> -->
     </div>
 
     <HowToLearn/>
