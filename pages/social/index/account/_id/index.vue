8ce669fb832097e0b2cbfa1a6b44f6d7437d5214
ngduy <duy.nv.96@gmail.com>
2020-05-26 11:27:48 +0700

    refactor code ui photo social account

--- pages/social/index/account/_id/index.vue
+++ pages/social/index/account/_id/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>{{ $route.name }}</div>
+  <div>
+    <section class="bg-white">
+      <account-cover/>
+      <account-menu active="timeline"/>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  import AccountCover from '~/components/page/social/account/AccountCover.vue'
+  import AccountMenu from '~/components/page/social/account/AccountMenu.vue'
+  
+  export default {
+    components: {
+      AccountCover,
+      AccountMenu
+    },
+    data() {
+      return {
+      
+      }
+    },
+    methods: {
+    
+    }
+  }
 </script>
 
 <style>
