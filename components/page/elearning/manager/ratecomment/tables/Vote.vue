6b32aae100973a7df08c0e04428392f52fe46f6d
ngduy <duy.nv.96@gmail.com>
2020-06-02 18:04:29 +0700

    fix display long text elearning rate page

--- components/page/elearning/manager/ratecomment/tables/Vote.vue
+++ components/page/elearning/manager/ratecomment/tables/Vote.vue
@@ -14,12 +14,14 @@
             <!--@mouseover="showCmt(row)"-->
             <!--@mouseleave="closeCmt"-->
         <!--&gt;-->
+        <!--tooltip-inner popover-inner-->
         <td class="cmt-content">
           <v-popover
-            class=""
+            popover-class="tooltip--eln-vote"
             trigger="hover"
             offset="10"
             placement="top"
+            popover-inner-class="tooltip-inner popover-inner dont-break-out"
           >
             <div>
               {{ row.content | truncStrFilter(30, false) }}
