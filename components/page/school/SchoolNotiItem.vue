33e11aa086545568da07a36fe9d975db104e1eeb
nguyenan <nguyenan1709@gmail.com>
2020-06-04 23:36:47 +0700

    update page school 2

---
+++ components/page/school/SchoolNotiItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="school-noti-item">
+        <div class="school-noti-item__img">
+            <img src="~assets/images/school/email-2.png" alt="">
+        </div>
+
+        <div class="school-noti-item__content">
+            <div class="text">{{ data.noti_title }}</div>
+            <div class="date">{{ data.date }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: {
+            type: Object,
+            default: {}
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/scss/components/school/_school-noti-item.scss";
+</style>
