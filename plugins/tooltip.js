8b21792f7fa72b6200b57bfe2394f506f0561a82
nguyenan <nguyenan1709@gmail.com>
2020-06-08 17:42:01 +0700

    css global tooltip & update modal chat page

--- plugins/tooltip.js
+++ plugins/tooltip.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { VTooltip, VPopover, VClosePopover } from 'v-tooltip'
+import '../assets/scss/plugin/_tooltip.scss';
 
 Vue.directive('tooltip', VTooltip)
 Vue.directive('close-popover', VClosePopover)
