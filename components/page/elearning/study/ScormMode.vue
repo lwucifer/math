f030f4771a7f009077091863c63ab2d640d9e244
Nguyen Tien Dat <tiendatleo218@gmail.com>
2020-06-10 20:34:53 +0700

    done UI SCORM mode

--- components/page/elearning/study/ScormMode.vue
+++ components/page/elearning/study/ScormMode.vue
@@ -5,15 +5,17 @@
       src="https://s3.cloud.cmctelecom.vn/dev-elearning-schoolly/scorm/20200610034529203_c2f56f5d1c170e0a9723d5bf2b149d67ecb42aee71513e8f3ca4013b236f82fd/Tin%20hoc%206_repair/index.htm"
     ></iframe>
 
-    <nav class="es-scorm-mode__menu">
-      <h3 class="es-scorm-mode__heading">
-        <button class="es-scorm-mode__button"></button>
+    <nav class="es-scorm-mode__menu" :class="{ show: showMenu }">
+      <h3 class="es-scorm-mode__heading" @click="showMenu = !showMenu">
+        <button class="es-scorm-mode__button">
+          <IconViewAgenda class="icon fill-opacity-1" />
+        </button>
         <span class="es-scorm-mode__title">Danh mục</span>
       </h3>
 
       <ul class="es-scorm-mode__menu-list">
         <li><a href="">Tên bài học 1</a></li>
-        <li><a href="">Tên bài học 2</a></li>
+        <li><a href="" class="active">Tên bài học 2</a></li>
         <li><a href="">Tên bài học 3</a></li>
         <li><a href="">Tên bài học 4</a></li>
       </ul>
@@ -22,7 +24,19 @@
 </template>
 
 <script>
-export default {};
+import IconViewAgenda from '~/assets/svg/v2-icons/view_agenda_24px.svg?inline';
+
+export default {
+  components: {
+    IconViewAgenda
+  },
+
+  data() {
+    return {
+      showMenu: false
+    }
+  }
+};
 </script>
 
 <style lang="scss">
