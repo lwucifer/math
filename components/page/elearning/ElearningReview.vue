87b2c5c29d0d7cd9e7aa2d39b86c6c7dade5ad3d
nguyenan <nguyenan1709@gmail.com>
2020-06-10 14:53:22 +0700

    fix css chi tiet bai giang

--- components/page/elearning/ElearningReview.vue
+++ components/page/elearning/ElearningReview.vue
@@ -6,9 +6,10 @@
         <div class="row items-center">
           <div class="col-md-6 col-sm-12">
             <div class="text-center d-inline-block">
-              <strong class="h1 color-primary">{{
+              <h1 class="text-primary" style="font-size: 3rem">{{
                 numeral(get(info, "voting.average_rate", 0)).format("0,0.[0]")
-              }}</strong>
+              }}</h1>
+
               <app-stars
                 :stars="Math.ceil(get(info, 'voting.average_rate', 0))"
                 :size="16"
