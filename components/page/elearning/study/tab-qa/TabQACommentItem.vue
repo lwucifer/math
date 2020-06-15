431cdfb7876eb9303f56adcd8683735e3a8e6a87
sonth95 <tranhongson1995@gmail.com>
2020-06-15 16:35:50 +0700

    update UI TabQACommentItem done

--- components/page/elearning/study/tab-qa/TabQACommentItem.vue
+++ components/page/elearning/study/tab-qa/TabQACommentItem.vue
@@ -29,11 +29,54 @@
 
       <div class="tab-qa-comment-item__content">
         <div v-if="!showInputUpdate" v-html="get(question, 'content', '')" class="word-break-all"></div>
-        <div v-else>
           <!-- show content update -->
+          <div v-else class="tab-qa-comment-editor" style="align-items: center">
+          <!-- <app-avatar :size="30" :src="get(user_login, 'avatar.low', '')" /> -->
+
+          <div class="tab-qa-comment-editor__right">
+            <div class="tab-qa-comment-editor__editor-wrapper">
+              <client-only>
+                <editor-content
+                  :editor="editor"
+                  class="editor tab-qa-comment-editor__editor"
+                  v-model="payload.content"
+                />
+              </client-only>
+
+              <app-upload
+                :fileList="uploadFileList"
+                accept="image/*, image/heic, image/heif"
+                class="tab-qa-comment-editor__upload d-inline-block"
+                @change="handleUploadChange"
+              >
+                <button class="tab-qa-comment-editor__btn">
+                  <IconCameraAlt class="icon d-block" />
+                </button>
+              </app-upload>
+            </div>
+
+            <!-- Upload Image -->
+            <div v-if="uploadImgSrc" class="tab-qa-comment-editor__preview">
+              <img :src="uploadImgSrc" alt />
+              <span
+                class="tab-qa-comment-editor__close-preview"
+                @click.stop="removeImgUpload"
+              >
+                <IconClose class="icon" />
+              </span>
+            </div>
+            
+            <!-- End Upload Image -->
+          </div>
+          <a class="action item-save" @click.prevent="handleUpdate">
+            <span>Lưu</span>
+          </a>
+          <a class="action item-cancel" @click.prevent="showInputUpdate = false">
+            <span>Huỷ</span>
+          </a>
         </div>
         <img
-          v-if="get(question, 'image_url', '')"
+          v-if="get(question, 'image_url', '') && !showInputUpdate"
           class="tab-qa-comment-item__img d-block"
           :src="get(question, 'image_url', '')"
           alt=""
@@ -52,15 +95,23 @@
         <button
           v-if="level == 1"
           class="tab-qa-comment-item__reply"
-          @click="showReply = !showReply"
+          @click="handleFeedBack"
         >
           Phản hồi
         </button>
-        <a class="action item-edit" @click.prevent="handleUpdate">
+        <a 
+          v-if="idToken.id == question.creator.id" 
+          class="action item-edit" 
+          @click.prevent="handleUpdate"
+        >
           <IconEdit class="icon fill-primary" />
           <span>Chỉnh sửa</span>
         </a>
-        <a class="action item-delete" @click.prevent="modalConfirmSubmit = true">
+        <a 
+          v-if="idToken.id == question.creator.id" 
+          class="action item-delete" 
+          @click.prevent="modalConfirmSubmit = true"
+        >
           <IconTrashAlt class="icon fill-secondary" />
           <span>Xóa</span>
         </a>
@@ -70,7 +121,7 @@
         title="Bạn chắc chắn muốn xoá bình luận"
         description="Bạn chắc chắn muốn xoá bình luận?"
         @cancel="modalConfirmSubmit = false"
-        @ok="handleQuestionSubmission(question)"
+        @ok="handleQuestionSubmission(question.id)"
         @close="modalConfirmSubmit = false"
       ></app-modal-confirm>
 
@@ -88,21 +139,38 @@ import QuestionLikeService from "~/services/elearning/study/QuestionLike";
 import InteractiveAnswer from "~/services/elearning/study/InteractiveAnswer";
 import IconEdit from "~/assets/svg/v2-icons/border_color_24px.svg?inline";
 import IconTrashAlt from "~/assets/svg/design-icons/trash-alt.svg?inline";
+import IconCameraAlt from "~/assets/svg/v2-icons/camera_alt_24px.svg?inline";
+import { Editor, EditorContent } from "tiptap";
+import { Placeholder, HardBreak, Mention, History } from "tiptap-extensions";
+import { EnterHandler } from "~/utils/tiptap-plugins";
+const IconClose = () => import("~/assets/svg/icons/close.svg?inline");
 
 export default {
   components: {
     IconThumbUp,
     IconAccessTime,
     IconEdit,
-    IconTrashAlt
+    IconTrashAlt,
+    IconCameraAlt,
+    Editor,
+    EditorContent,
+    IconClose
   },
 
   data() {
     return {
+      idToken: "",
       showReply: false,
       submit: true,
       modalConfirmSubmit: false,
       showInputUpdate: false,
+      editor: null,
+      uploadFileList: [],
+      uploadImgSrc: get(this, "question.image_url", ""),
+      payload: {
+        content: "",
+        question_id: get(this, "question.id", ""),
+      },
     };
   },
 
@@ -123,7 +191,42 @@ export default {
     },
   },
 
+  mounted() {
+    // Init editor
+    this.editor = new Editor({
+      content: this.question.content || "",
+      autoFocus: true,
+      extensions: [
+        new Placeholder({
+          showOnlyCurrent: true,
+          showOnlyWhenEditable: true,
+          emptyNodeText: "Viết bình luận",
+        }),
+        new HardBreak(),
+        new History(),
+        new EnterHandler({
+          onEnter: this.submit,
+        }),
+      ],
+      onPaste: this.handleEditorPaste,
+    });
+    this.idToken = JSON.parse(localStorage.getItem("token_user_schoolly"));
+  },
+  created() {
+  },
+
   methods: {
+    handleFeedBack(){
+      this.showReply = !this.showReply;
+      this.showInputUpdate = false;
+    },
+    handleUpdate(){
+      this.showInputUpdate = true;
+      this.showReply = false;
+      this.$nextTick(() => {
+        this.editor.focus()
+      })
+    },
     handleLike() {
       if (this.level == 1) {
         this.likeQuestion();
@@ -184,11 +287,17 @@ export default {
         options
       );
     },
-    handleUpdate(){
-      this.showInputUpdate = true;
+    handleUploadChange(fileList, event) {
+      // this.image = fileList[0];
+      // this.uploadFileList = Array.from(fileList);
+      // getBase64(this.uploadFileList[0], (src) => {
+      //   this.uploadImgSrc = src;
+      // });
     },
-    handleQuestionSubmission(question) {
-      console.log('handleQuestionSubmission', question)
+    
+    handleQuestionSubmission(_id) {
+      console.log('handleQuestionSubmission', _id)
+      this.modalConfirmSubmit = false;
     },
     get,
     numeral,
@@ -198,4 +307,5 @@ export default {
 
 <style lang="scss">
 @import "~/assets/scss/components/elearning/study/_tab-qa-comment-item.scss";
+@import "~/assets/scss/components/elearning/study/_tab-qa-comment-editor.scss";
 </style>
