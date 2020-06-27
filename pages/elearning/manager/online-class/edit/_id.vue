eddaea3ec2a045aab487da1a849337270cc24c6b
Nguyen Quang Trung <quangtrung.uet@gmail.com>
2020-06-27 15:33:42 +0700

    add isChangeLabelColor app-checkbox

--- pages/elearning/manager/online-class/edit/_id.vue
+++ pages/elearning/manager/online-class/edit/_id.vue
@@ -123,36 +123,43 @@
                             @change="check($event, 'MON', index)"
                             :checked="checkIncules(selectedItems[index],('MON'))"
                             label="Thứ 2"
+                            :isChangeLabelColor="true"
                           ></app-checkbox>
                           <app-checkbox
                             @change="check($event, 'TUE', index)"
                             :checked="checkIncules(selectedItems[index],('TUE'))"
                             label="Thứ 3"
+                            :isChangeLabelColor="true"
                           />
                           <app-checkbox
                             @change="check($event, 'WED', index)"
                             :checked="checkIncules(selectedItems[index],('WED'))"
                             label="Thứ 4"
+                            :isChangeLabelColor="true"
                           />
                           <app-checkbox
                             @change="check($event, 'THU', index)"
                             :checked="checkIncules(selectedItems[index],('THU'))"
                             label="Thứ 5"
+                            :isChangeLabelColor="true"
                           />
                           <app-checkbox
                             @change="check($event, 'FRI', index)"
                             :checked="checkIncules(selectedItems[index],('FRI'))"
                             label="Thứ 6"
+                            :isChangeLabelColor="true"
                           />
                           <app-checkbox
                             @change="check($event, 'SAT', index)"
                             :checked="checkIncules(selectedItems[index],('SAT'))"
                             label="Thứ 7"
+                            :isChangeLabelColor="true"
                           />
                           <app-checkbox
                             @change="check($event, 'SUN', index)"
                             :checked="checkIncules(selectedItems[index],('SUN'))"
                             label="Chủ nhật"
+                            :isChangeLabelColor="true"
                           />
                         </div>
                       </div>
