38d5fab07c3a30c8d7def3f346a9515e5c69c388
Nguyen Quang Trung <quangtrung.uet@gmail.com>
2020-06-28 16:41:28 +0700

    <br> danh cho....

--- components/page/home/HomeBanner.vue
+++ components/page/home/HomeBanner.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="home-banner__title">Học tập mọi lúc, mọi nơi <span>với Schoolly.</span></div>
 
-      <div class="home-banner__text">Nền tảng học tập trực tuyến toàn diện dành cho<span> học sinh, giáo viên, nhà trường và các đơn vị giáo dục.</span></div>
+      <div class="home-banner__text">Nền tảng học tập trực tuyến toàn diện <br>dành cho học sinh, giáo viên, nhà trường và các đơn vị giáo dục.</div>
 
       <app-button
         size="md"
