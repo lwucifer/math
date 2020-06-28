06316b94306d40b941411e31444a76f0ec7b36c3
ngduy <duy.nv.96@gmail.com>
2020-06-28 14:28:42 +0700

    update list elearning of school

--- components/page/school/Courses/SchoolCoursesSearch.vue
+++ components/page/school/Courses/SchoolCoursesSearch.vue
@@ -12,21 +12,31 @@
                 >
               </filter-button>
               <div class="d-flex w-100" v-if="filterSelect">
-                  <app-vue-select
-                    class="app-vue-select w-100"
-                    placeholder="Học phí"
-                    has-border
-                  />
-                  <app-vue-select
-                    class="app-vue-select w-100"
-                    placeholder="Thời lượng"
-                    has-border
-                  />
-                  <app-vue-select
-                    class="app-vue-select w-100"
-                    placeholder="Trình độ"
-                    has-border
-                  />
+                <app-select
+                    bordered
+                    v-model="params.fee"
+                    @change="handleChangeFee"
+                    :options="feeOpts"
+                    placeholder="Học phí"
+                    size="sm"
+                />
+                <app-select
+                    bordered
+                    v-model="params.time"
+                    @change="handleChangeTimes"
+                    :options="timeOpts"
+                    placeholder="Thời lượng"
+                    size="sm"
+                />
+                <app-select
+                    bordered
+                    v-model="params.level"
+                    @change="handleChangeLevel"
+                    :options="levelOpts"
+                    placeholder="Trình độ"
+                    size="sm"
+                
+                />
               </div>
           </div>
           <div class="sort-courses-school">
@@ -56,22 +66,34 @@
             </n-link>
         </span>
       </div>
-      <div class="row list-course-school-search" v-if="tab=='1'">
+
+        <div v-if="loadingElearning" class="container mt-6">
+            <div class="row">
+                <div v-for="i in 16" :key="i" class="col-md-3 mb-6">
+                    <div class="bg-white py-6 px-3">
+                        <VclList />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      <div class="row list-course-school-search" v-else-if="tab=='1'">
         <div
-          v-for="item in newestCourse && newestCourse.content || []"
-          :key="item.id"
-          class="col-md-3"
-        >
-            <CourseItem2 :item="item" :size="'sm'" />
+            v-for="item in courses && courses.content || []"
+            :key="item.id"
+            class="col-md-3"
+            >
+                <CourseItem2 :item="item" :size="'sm'" />
         </div>
         <app-pagination
             :pagination="pagination"
             class="mt-5 w-100"
         />
       </div>
+
       <div class="row list-course-school-search" v-if="tab=='2'">
         <div
-          v-for="item in newestLecture && newestLecture.content || []"
+          v-for="item in lectures && lectures.content || []"
           :key="item.id"
           class="col-md-3"
         >
@@ -87,13 +109,17 @@
 
 <script>
 import CourseItem2 from "~/components/page/course/CourseItem2";
+import { VclList } from "vue-content-loading";
 import { mapState } from "vuex";
 import * as actionTypes from "~/utils/action-types";
 import { ELEARNING_TYPES, SUBJECT_CODE } from "~/utils/constants";
 import { get } from "lodash";
+import { optionSelectSubject } from "~/utils/common";
+
 export default {
     components:{
-        CourseItem2
+        CourseItem2,
+        VclList
     },
     props:{
         typeSearch:{
@@ -112,51 +138,166 @@ export default {
                 first: 1,
                 last: 0,
                 number: 1
-            }
+            },
+            params: {
+                elearning_type: ELEARNING_TYPES.COURSE,
+                school_id: null,
+                subject: null,
+                teacher: null,
+                fee: null,
+                level: null,
+                school_id: null,
+                page: 1,
+                size: 12,
+            },
+            loadingElearning: false,
+            feeOpts: [
+                { value: null, text: "Tất cả" },
+                { value: 1, text: "Miễn phí" },
+                { value: 0, text: "Có phí" }
+            ],
+            timeOpts: [
+                { value: null, text: "Tất cả" },
+                { value: "0h-1h", text: "0 - 1 giờ" },
+                { value: "1h-2h", text: "1- 2 giờ" },
+                { value: "2h-4h", text: "2 - 4 giờ" },
+                { value: "4h+", text: "Trên 4 giờ" }
+            ],
         }
     },
     methods:{
         handleShowFilter(){
             this.filterSelect =!this.filterSelect;
         },
-        fetchNewestLecture(){
-            const rs = this.$store.dispatch(
-                `elearning/public/public-newest/${actionTypes.ELEARNING_PUBLIC_NEWEST.LIST_LECTURE}`,
+        async fetchLectures(){
+            const params = this.params
+            const rs = await this.$store.dispatch(
+                `elearning/school/school-elearning/${actionTypes.SCHOOL_ELEARNING.LIST}`,
                 {
-                params: {
-                    type: ELEARNING_TYPES.LECTURE
-                }
+                    params
                 }
             );
+            this.updatePagination()
         },
-        fetchtNewestCourse(){
-            const res = this.$store.dispatch(
-                `elearning/public/public-newest/${actionTypes.ELEARNING_PUBLIC_NEWEST.LIST_COURSE}`,
-                {
-                params: {
-                    type: ELEARNING_TYPES.COURSE
-                }
-                }
+        async fetchCourses(){
+            try {
+                this.loadingElearning = true
+                const params = this.params
+                const rs = await this.$store.dispatch(
+                    `elearning/school/school-elearning/${actionTypes.SCHOOL_ELEARNING.LIST}`,
+                    {
+                        params
+                    }
+                );
+                this.updatePagination()
+            } catch(error) {
+
+            } finally {
+                this.loadingElearning = false
+            }
+        },
+        async fetchLevels() {
+            await this.$store.dispatch(
+                `elearning/public/public-levels/${actionTypes.ELEARNING.LEVEL}`
             );
         },
         changeTab(val){
             this.tab = val;
         },
+        initParams(elearningType = ELEARNING_TYPES.LECTURE) {
+            this.params = {
+                elearning_type: ELEARNING_TYPES.COURSE,
+                school_id: this.$route.params.id,
+                teacher: null,
+                fee: null,
+                level: null,
+                page: 1,
+                size: 12,
+            }
+        },
+        updateFilter(data) {
+            this.params = { ...data }
+        },
+        updatePagination() {
+            let paginationData;
+            if (this.tab == 1) {
+                paginationData = this.courses || {}
+            } else if (this.tab == 2) {
+                this.paginationData = this.lectures || {}
+            } else {
+                this.paginationData = {}
+            }
+            this.pagination = {
+                total_pages: get(paginationData, "total_pages", 0),
+                size: get(paginationData, "size", 10),
+                total_elements: get(paginationData, "total_elements", 0),
+                first: get(paginationData, "first", false),
+                last: get(paginationData, "last", false),
+                number: get(paginationData, "number", 0)
+            };
+        },
+        async refreshData() {
+            this.params.page = 1
+            await this.fetchCourses()
+        },
+        handleChangeFee(_newVal, _selectedVal) {
+            console.log("[handleChangeFee]", _newVal, _selectedVal);
+            this.updateFilter({ fee: _newVal });
+            this.refreshData();
+        },
+
+        handleChangeTimes(_newVal, _selectedVal) {
+            console.log("[handleChangeTimes]", _newVal, _selectedVal);
+            this.updateFilter({ time: _newVal });
+            this.refreshData();
+        },
+
+        handleChangeLevel(_newVal, _selectedVal) {
+            console.log("[handleChangeLevel]", _newVal, _selectedVal);
+            this.updateFilter({ level: _newVal });
+            this.refreshData();
+        },
         get
     },
     computed:{
-        ...mapState("elearning/public/public-newest", [
-            "newestLecture",
-            "newestCourse"
-        ]),
+        ...mapState("elearning/public/public-levels", {
+            levels: "levels"
+        }),
+        ...mapState("elearning/school/school-elearning", {
+            lectures: "lecture",
+            courses: "course"
+        }),
+        levelOpts() {
+            const alls = optionSelectSubject(this.levels);
+            return alls.map(c => {
+                return {
+                value: c.id,
+                text: c.name
+                };
+            });
+        },
     },
-    created(){
-        this.fetchNewestLecture();
-        this.fetchtNewestCourse();
+    async created(){
+        this.initParams()
+        await Promise.all([
+            this.fetchLevels(),
+            this.fetchLectures(),
+            this.fetchCourses()
+        ])
     },
 }
 </script>
 
 <style lang="scss">
-
+    .wrap-courses-school {
+        .filter-form-school {
+            .app-select {
+                margin-right: 0.6rem;
+                margin-left: 0.6rem;
+            }
+            .btn--filter {
+                margin-right: 0.6rem;
+            }
+        }
+    }
 </style>
