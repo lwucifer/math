cba3839e4132ba56c2a15adf9d4664927f35229f
luunguyen <luunguyen.mta@gmail.com>
2020-07-15 16:43:21 +0700

    add

--- pages/about.vue
+++ pages/about.vue
@@ -1,41 +1,55 @@
 <template>
-  <div class="hello">
-    <textarea v-model="formula" cols="30" rows="10"></textarea>
-  <vue-mathjax :formula="formula"></vue-mathjax>
+  <div>
+    <quasar-tiptap v-bind="options" @update="onUpdate" />
   </div>
 </template>
-
+ 
 <script>
-import { VueMathjax } from 'vue-mathjax'
+import CodeMirror from 'codemirror';
+import { QuasarTiptap, RecommendedExtensions } from 'quasar-tiptap'
+import 'quasar-tiptap/lib/index.css'
+
+CM = document.getElementById('CMEditor');
+CM.CodeMirror.toTextArea();
+ 
 export default {
-  components: {
-    'vue-mathjax': VueMathjax
-  },
-  name: 'HelloWorld',
   data () {
     return {
-      formula: '$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$',
-      msg: 'Welcome to Your Vue.js App'
+      options: {
+        content: 'content',
+        editable: true,
+        extensions: [
+          ...RecommendedExtensions,
+          // other extenstions
+          // name string, or custom extension
+        ],
+        toolbar: [
+          'add-more',
+          'separator',
+          'bold',
+          'italic',
+          'underline',
+          // other toolbar buttons
+          // name string
+        ]
+      },
+      json: '',
+      html: ''
     }
+  },
+  components: {
+    QuasarTiptap,
+  },
+  methods: {
+    onUpdate ({ getJSON, getHTML }) {
+      this.json = getJSON()
+      this.html = getHTML()
+      console.log('html', this.html)
+    }
+  },
+  mounted () {
+    // set language dynamically
+    this.$o.lang.set('en-us')
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
